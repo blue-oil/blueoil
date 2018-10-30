7118e88ffff14832af27dc9f36db910d8846bb80
Yoshihiro Yamazaki <yoya@awm.jp>
2018-10-31 00:00:48 +0900

    - image Resize with C++ - OpenCV glue

---
+++ runtime/src/blueoil_image.cpp
@@ -0,0 +1,190 @@
+/* Copyright 2018 Leapmind Inc. */
+
+#include <iostream>
+#include <cmath>
+
+#include "blueoil.hpp"
+#include "blueoil_image.hpp"
+
+namespace blueoil {
+namespace image {
+
+template <typename T>
+T clamp(const T x, const T lowerLimit, const T upperLimit) {
+    if (x < lowerLimit) {
+	return lowerLimit;
+    }
+    if (upperLimit < x) {
+	return upperLimit;
+    }
+    return x;
+}
+
+/*
+ * return RGB float array
+ */
+float *Tensor_at(Tensor &tensor, const int x, const int y) {
+    const int height = tensor.shape[0];
+    const int width  = tensor.shape[1];
+    const int channels  = tensor.shape[2];
+    if ((channels != 1) && (channels != 3)) {
+	throw "wrong channles != 1,3"; // XXX
+    }
+    const int clamped_x = clamp(x, 0, width-1);
+    const int clamped_y = clamp(y, 0, height-1);
+    const int scanlineSize = width * channels;
+    float *imagePtr = &(tensor.data[0]);
+    float *scanlinePtr = imagePtr + clamped_y * scanlineSize;
+    float *pixelPtr = scanlinePtr + clamped_x * channels;
+    return pixelPtr;
+}
+
+Tensor Tensor_CHW_to_HWC(Tensor &tensor) {
+    const int channels  = tensor.shape[0];
+    const int height = tensor.shape[1];
+    const int width  = tensor.shape[2];
+    if ((channels != 1) && (channels != 3)) {
+	throw "wrong channles != 1,3"; // XXX
+    }
+    Tensor dstTensor = Tensor::zeros({height, width, channels});
+    int srcPlaneSize = width * height;
+    float *srcImagePtr = &(tensor.data[0]);
+    float *dstImagePtr = &(dstTensor.data[0]);
+    for (int y = 0 ; y < height ; y++) {
+	for (int x = 0 ; x < width ; x++) {
+	    float *srcPixelPtr0 = srcImagePtr + x + (y * height);
+	    for (int c = 0 ; c < channels ; c++) {
+		float *srcPixelPtr = srcPixelPtr0 + (c * srcPlaneSize);
+		*dstImagePtr = *srcPixelPtr;
+		dstImagePtr++;
+	    }
+	}
+    }
+    return dstTensor;
+}
+
+Tensor Tensor_HWC_to_CHW(Tensor &tensor) {
+    int height = tensor.shape[0];
+    int width  = tensor.shape[1];
+    int channels  = tensor.shape[2];
+    if ((channels != 1) && (channels != 3)) {
+	throw "wrong channles != 1,3"; // XXX
+    }
+    Tensor dstTensor = Tensor::zeros({channels, height, width});
+    float *srcImagePtr = &(tensor.data[0]);
+    float *dstImagePtr = &(dstTensor.data[0]);
+    for (int c = 0 ; c < channels ; c++) {
+	float *srcPixelPtr = srcImagePtr + c;
+	for (int y = 0 ; y < height ; y++) {
+	    for (int x = 0 ; x < width ; x++) {
+		*dstImagePtr = *srcPixelPtr;
+		srcPixelPtr += 3;
+		dstImagePtr++;
+	    }
+	}
+    }
+    return dstTensor;
+}
+
+/*
+ * Resize Image
+ */
+Tensor ResizeHorizontal(Tensor &tensor, const int width,
+			const enum ResizeFilter filter) {
+    if (filter != RESIZE_FILTER_NEAREST_NEIGHBOR) {
+	throw "unknown ResizeFilter:"; // XXX
+    }
+    const int srcHeight = tensor.shape[0];
+    const int srcWidth  = tensor.shape[1];
+    const int channels  = tensor.shape[2];
+    const int height = srcHeight;
+    Tensor dstTensor = Tensor::zeros({height, width, channels});
+    float xScale = (float) width / (float) srcWidth;
+    int xSrcWindow = floorf(1/xScale);
+    xSrcWindow = 1; // Nearest
+    //xSrcWindow = (xSrcWindow < 2)? 2 :xSrcWindow; // Bi-Liner
+    for (int dstY = 0 ; dstY < height ; dstY++) {
+	for (int dstX = 0 ; dstX < width ; dstX++) {
+	    int srcX = (int) floorf(dstX/xScale);
+	    int srcY = dstY;
+	    float r = 0.0, g = 0.0, b = 0.0;
+	    float totalW = 0.0;
+	    for (int x = -xSrcWindow ; x < xSrcWindow; x++){
+		float *srcRGB = blueoil::image::Tensor_at(tensor,
+							srcX + x, srcY);
+		float d = fabs((float)x / (float)xSrcWindow);
+		float w = (d<0.5)?1.0:0.0; // NearestNeighbor
+		// float w = 1.0 - d; // Bi-Linear
+		r += w * srcRGB[0];
+		g += w * srcRGB[1];
+		b += w * srcRGB[2];
+		totalW += w;
+	    }
+	    float *dstRGB = blueoil::image::Tensor_at(dstTensor, dstX, dstY);
+	    dstRGB[0] = r / totalW;
+	    dstRGB[1] = g / totalW;
+	    dstRGB[2] = b / totalW;
+	}
+    }
+    return dstTensor;
+}
+
+Tensor ResizeVertical(Tensor &tensor, const int height,
+		      const enum ResizeFilter filter) {
+    if (filter != RESIZE_FILTER_NEAREST_NEIGHBOR) {
+	throw "unknown ResizeFilter:"; // XXX
+    }    
+    const int srcHeight = tensor.shape[0];
+    const int srcWidth  = tensor.shape[1];
+    const int channels  = tensor.shape[2];
+    const int width = srcWidth;
+    Tensor dstTensor = Tensor::zeros({height, width, channels});
+    float yScale = (float) height / (float) srcHeight;
+    int ySrcWindow = floorf(1/yScale);
+    ySrcWindow = 1; // Nearest
+    // ySrcWindow = (ySrcWindow < 2)? 2 :ySrcWindow; // Bi-Linear
+    for (int dstY = 0 ; dstY < height ; dstY++) {
+	for (int dstX = 0 ; dstX < width ; dstX++) {
+	    int srcX = dstX;
+	    int srcY = (int) floorf(dstY/yScale);
+	    float r = 0.0, g = 0.0, b = 0.0;
+	    float totalW = 0.0;
+	    for (int y = -ySrcWindow ; y < ySrcWindow ; y++) {
+		float *srcRGB = blueoil::image::Tensor_at(tensor,
+							srcX, srcY + y);
+		float d = fabs((float)y / (float)ySrcWindow);
+		float w = (d<0.5)?1.0:0.0; // NearestNeighbor
+		// float w = 1.0 - d; // Bi-Linear
+		r += w * srcRGB[0];
+		g += w * srcRGB[1];
+		b += w * srcRGB[2];
+		totalW += w;
+	    }
+	    float *dstRGB = blueoil::image::Tensor_at(dstTensor, dstX, dstY);
+	    dstRGB[0] = r / totalW;
+	    dstRGB[1] = g / totalW;
+	    dstRGB[2] = b / totalW;
+	}
+    }
+    return dstTensor;
+} 
+
+Tensor Resize(const Tensor& image, const int width, const int height,
+	      const enum ResizeFilter filter) {
+    if (filter != RESIZE_FILTER_NEAREST_NEIGHBOR) {
+	throw "unknown ResizeFilter:"; // XXX
+    }
+    const int srcHeight = image.shape[0];
+    const int srcWidth  = image.shape[1];
+    Tensor dstImage = image;
+    if  (srcWidth != width) {
+	dstImage = ResizeHorizontal(dstImage, width, filter);
+    }
+    if  (srcHeight != height) {
+	dstImage = ResizeVertical(dstImage, height, filter);
+    }
+    return dstImage;
+}   
+
+} // namespace image
+} // namespace blueoil
---
+++ runtime/src/blueoil_opencv.cpp
@@ -0,0 +1,76 @@
+/* Copyright 2018 Leapmind Inc. */
+
+#include "blueoil.hpp"
+#include "blueoil_image.hpp"
+#include "blueoil_opencv.hpp"
+
+namespace blueoil {
+namespace opencv {
+
+/*
+ * accept BGR OpenCV Mat images (not RGB)
+ */
+Tensor Tensor_fromCVMat(cv::Mat img) {
+    int width = img.cols;
+    int height = img.rows;
+    int channels = img.elemSize();
+    if ((channels != 1) && (channels != 3)) {
+	throw "wrong channles != 1,3"; // XXX
+    }
+    blueoil::Tensor tensor = blueoil::Tensor::zeros({height, width, channels});
+    for (int y = 0 ; y < height ; y++) {
+	for (int x = 0 ; x < width ; x++) {
+	    float *tensorPixel = blueoil::image::Tensor_at(tensor, x, y);
+	    uchar *imgPixel = &(img.data[ y * img.step + x * img.elemSize()]);
+	    if (channels == 1) {
+		tensorPixel[0] = imgPixel[0]; // I (grayscale)
+	    } else if (channels == 3) {
+		tensorPixel[0] = imgPixel[2]; // R
+		tensorPixel[1] = imgPixel[1]; // G
+		tensorPixel[2] = imgPixel[0]; // B
+	    } else {
+		throw "wrong channles != 1,3"; // XXX
+	    }
+	}
+    }
+    //
+    return tensor;
+}
+
+/*
+ * generate BGR OpenCV Mat images (not RGB)
+ */
+cv::Mat Tensor_toCVMat(Tensor &tensor) {
+    int height = tensor.shape[0];
+    int width  = tensor.shape[1];
+    int channels = tensor.shape[2];
+    cv::Mat img;
+    if (channels == 1) {
+	img = cv::Mat::zeros(height, width, CV_8U);   // uchar[1] grayscale
+    } else if (channels == 3) {
+	img = cv::Mat::zeros(height, width, CV_8UC3); // uchar[3] rgb color
+    } else {
+	throw "wrong channles != 1,3"; // XXX
+    }
+    for (int y = 0 ; y < height ; y++) {
+	for (int x = 0 ; x < width ; x++) {
+	    float *tensorPixel = blueoil::image::Tensor_at(tensor, x, y);
+	    uchar *imgPixel = &(img.data[ y * img.step + x * img.elemSize()]);
+	    if (channels == 1) {
+		imgPixel[0] = tensorPixel[0]; // I (grayscale)
+	    } else if (channels == 3) {
+		imgPixel[2] = tensorPixel[0]; // R
+		imgPixel[1] = tensorPixel[1]; // G
+		imgPixel[0] = tensorPixel[2]; // B
+	    } else {
+		throw "wrong channles != 1,3"; // XXX
+	    }
+	}
+    }
+    return img;
+}
+
+}  // namespace opencv
+}  // namespace blueoil
+
+
