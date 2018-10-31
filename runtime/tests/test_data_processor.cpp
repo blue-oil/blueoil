379c55373e2fd9b9d10eade95c7109b16b98bd29
Yoshihiro Yamazaki <yoya@awm.jp>
2018-10-31 14:59:24 +0900

    blueoil data_processor resize implementation. (join to blueoil image Resize)

---
+++ runtime/tests/test_data_processor.cpp
@@ -0,0 +1,91 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "blueoil.hpp"
+#include "blueoil_image.hpp"
+#include "blueoil_opencv.hpp"
+#include "blueoil_data_processor.hpp"
+
+float test_input[3][8][8] =
+    { { // Red
+       {255, 0, 0, 0, 0, 0, 0, 0},
+       {0, 255, 0, 0, 0, 0, 0, 0},
+       {0, 0, 100, 0, 0, 0, 0, 0},
+       {0, 0, 0, 100, 0, 0, 0, 0},
+       {0, 0, 0, 0, 100, 0, 0, 0},
+       {0, 0, 0, 0, 0, 100, 0, 0},
+       {0, 0, 0, 0, 0, 0, 255, 0},
+       {0, 0, 0, 0, 0, 0, 0, 255}
+       },
+      { // Green
+       {0, 0, 0, 0, 0, 0, 0, 255},
+       {0, 0, 0, 0, 0, 0, 255, 0},
+       {0, 0, 0, 0, 0, 100, 0, 0},
+       {0, 0, 0, 0, 100, 0, 0, 0},
+       {0, 0, 0, 100, 0, 0, 0, 0},
+       {0, 0, 100, 0, 0, 0, 0, 0},
+       {0, 255, 0, 0, 0, 0, 0, 0},
+       {255, 0, 0, 0, 0, 0, 0, 0}
+      },
+      { // Blue
+       {  0,  0,  0,255,255,  0,  0,  0},
+       {  0,  0,  0,255,255,  0,  0,  0},
+       {  0,  0,  0,255,255,  0,  0,  0},
+       {255,255,255,100,100,255,255,255},
+       {255,255,255,100,100,255,255,255},
+       {  0,  0,  0,255,255,  0,  0,  0},
+       {  0,  0,  0,255,255,  0,  0,  0},
+       {  0,  0,  0,255,255,  0,  0,  0}
+      } };
+
+float test_expect[3][4][4] =
+    { { // Red
+       {255,  0, 0,   0},
+       {  0,100, 0,   0},
+       {  0,  0,100,  0},
+       {  0,  0,  0,255}
+       },
+      { // Green
+       {  0,  0,  0,  0},
+       {  0,  0,  0,  0},
+       {  0,  0,  0,  0},
+       {  0,  0,  0,  0}
+      },
+      { // Blue
+       {  0,  0,255,  0},
+       {  0,  0,255,  0},
+       {255,255,100,255},
+       {  0,  0,255,  0}
+      } };
+
+int test_data_processor_resize() {
+    // CWH (3-channel, height, width)
+    int width = 4, height = 4;
+    const std::pair<int, int>& image_size = std::make_pair(width, height);
+    blueoil::Tensor input = blueoil::Tensor::array({3, 8, 8},
+						   (float *)test_input);
+    blueoil::Tensor expect = blueoil::Tensor::array({3, 4, 4} ,
+						    (float *)test_expect);
+    input = blueoil::image::Tensor_CHW_to_HWC(input);
+    expect = blueoil::image::Tensor_CHW_to_HWC(expect);
+    blueoil::Tensor output = blueoil::data_processor::Resize(input,
+							     image_size);
+    if (! output.allclose(expect)) {
+	std::cerr << "test_data_processor_resize: output != expect" << std::endl;
+	output = blueoil::image::Tensor_HWC_to_CHW(output);
+	expect = blueoil::image::Tensor_HWC_to_CHW(expect);
+	output.dump();
+	expect.dump();
+	return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+int main(void) {
+    int status_code = 0;
+    status_code = test_data_processor_resize();
+    if (status_code != EXIT_FAILURE) {
+	std::exit(status_code);
+    }
+    std::exit(EXIT_SUCCESS);
+}
