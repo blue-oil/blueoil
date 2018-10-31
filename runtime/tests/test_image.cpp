cc45a9e3f8f3c09a88a5f0799cc94da43a0358f5
Yoshihiro Yamazaki <yoya@awm.jp>
2018-10-31 14:39:19 +0900

    tensor image routine unit test.

---
+++ runtime/tests/test_image.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <iostream>
+#include "blueoil.hpp"
+#include "blueoil_image.hpp"
+
+float test_chw_data[3][2][2] =  // planar RGB format
+    { {  // Red
+       {255,255},
+       {  0,  0},
+       },
+      {  // Green
+       {  0,255},
+       {  0,255},
+      },
+      {  // Blue
+       {  0,  0},
+       {255,  0},
+      } };
+
+
+float test_hwc_data[2][2][3] =  // packed RGB format
+    {
+     { {255,  0,  0}, {255,255,  0} },  // R Y
+     { {  0,  0,255}, {  0,255,  0} },  // B G
+    };
+
+int test_image() {
+    blueoil::Tensor test_chw = blueoil::Tensor::array({3, 2, 2},
+						      (float *) test_chw_data);
+    blueoil::Tensor test_hwc = blueoil::Tensor::array({2, 2, 3},
+						      (float *) test_hwc_data);
+    blueoil::Tensor test_chw_hwc = blueoil::image::Tensor_CHW_to_HWC(test_chw);
+    blueoil::Tensor test_hwc_chw = blueoil::image::Tensor_HWC_to_CHW(test_hwc);
+    if (test_chw.allequal(test_hwc_chw) == false) {
+	std::cerr << "test_image: test_chw != test_hwc_chw" << std::endl;
+	test_chw.dump();
+	test_hwc_chw.dump();
+	return EXIT_FAILURE;
+    }
+
+    if (test_hwc.allequal(test_chw_hwc) == false) {
+	std::cerr << "test_image: test_hwc != test_chw_hwc" << std::endl;
+	test_hwc.dump();
+	test_chw_hwc.dump();
+	return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int main(void) {
+    int status_code = test_image();
+    std::exit(status_code);
+}
+
