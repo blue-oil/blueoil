bf6825dbe58dd3d56ced4a8be6568147558963a1
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2018-11-02 19:47:22 +0900

    Add sphinx.ext.napoleon to parse google style docstring

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
