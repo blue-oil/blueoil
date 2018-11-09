359bff8409fd248e1a4683dea977a31e2c044093
tkclimb <tkclimb0911@gmail.com>
2018-11-09 13:17:35 +0900

    Implement quantized convolution with kn2row and tiling

--- dlk/backends/src/cpp/conv_kn2row_tiling.cpp
+++ dlk/backends/src/cpp/conv_kn2row_tiling.cpp
@@ -1,8 +1,7 @@
 #include "common/global.h"
 #include "cpp/utils.h"
 
-namespace cpp
-{
+namespace cpp {
 namespace p = conv_kn2row_params;
 
 /// estimated size buffer size
@@ -16,8 +15,7 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
                              const unsigned out_w, const unsigned out_h,
                              const unsigned out_c, const unsigned k_w,
                              const unsigned k_h, const unsigned pad,
-                             const unsigned stride)
-{
+                             const unsigned stride) {
   /// just alias for better understanding
   static const unsigned out_c_low = p::num_pe;
   assert((out_c % out_c_low) == 0);
@@ -28,26 +26,20 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
   assert(k_w <= p::max_k_w);
   assert(k_w >= p::min_k_w);
 
-  for (int oc_high = 0; oc_high < out_c; oc_high += out_c_low)
-  {
+  for (int oc_high = 0; oc_high < out_c; oc_high += out_c_low) {
     T_out threshold_buf[out_c_low][p::num_thresholds];
 
-    if (threshold_data != NULL)
-    {
-      for (unsigned oc = 0; oc < out_c_low; oc++)
-      {
-        for (unsigned i = 0; i < p::num_thresholds; i++)
-        {
+    if (threshold_data != NULL) {
+      for (unsigned oc = 0; oc < out_c_low; oc++) {
+        for (unsigned i = 0; i < p::num_thresholds; i++) {
           unsigned idx_th = (oc_high + oc) * p::num_thresholds + i;
           threshold_buf[oc][i] = threshold_data[idx_th];
         }
       }
     }
 
-    for (int ih_high = 0; ih_high < in_h + 2 * pad; ih_high += p::tile_h)
-    {
-      for (int iw_high = 0; iw_high < in_w + 2 * pad; iw_high += p::tile_w)
-      {
+    for (int ih_high = 0; ih_high < in_h + 2 * pad; ih_high += p::tile_h) {
+      for (int iw_high = 0; iw_high < in_w + 2 * pad; iw_high += p::tile_w) {
         T_in in_buf[p::in_tile_h][p::in_tile_w][p::max_in_c];
         T_out out_buf[p::tile_w][p::tile_w][out_c_low];
         T_k k_buf[p::max_in_c][out_c_low];
@@ -56,10 +48,8 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
         //           << std::endl;
 
         /// preload input
-        for (int ih_low = 0; ih_low < p::in_tile_h; ++ih_low)
-        {
-          for (int iw_low = 0; iw_low < p::in_tile_w; ++iw_low)
-          {
+        for (int ih_low = 0; ih_low < p::in_tile_h; ++ih_low) {
+          for (int iw_low = 0; iw_low < p::in_tile_w; ++iw_low) {
             /// index must care the padding, so we skip the padding part that
             /// doesn't exist in actuall memory.
             int ih = (ih_low + ih_high - pad);
@@ -67,8 +57,7 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
             bool input_on =
                 (ih >= 0) && (iw >= 0) && (ih < in_h) && (iw < in_w);
 
-            for (int ic = 0; ic < in_c; ic++)
-            {
+            for (int ic = 0; ic < in_c; ic++) {
               int idx_in = ih * in_w * in_c + iw * in_c + ic;
               in_buf[ih_low][iw_low][ic] =
                   (input_on) ? in_data[idx_in] : T_in(0);
@@ -81,27 +70,20 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
         /// initialize output_buf
         /// TODO: this could be done at the same time in the accumuratoin
         /// step.
-        for (int oh = 0; oh < p::tile_h; ++oh)
-        {
-          for (int ow = 0; ow < p::tile_w; ++ow)
-          {
-            for (int oc = 0; oc < out_c_low; ++oc)
-            {
+        for (int oh = 0; oh < p::tile_h; ++oh) {
+          for (int ow = 0; ow < p::tile_w; ++ow) {
+            for (int oc = 0; oc < out_c_low; ++oc) {
               out_buf[oh][ow][oc] = 0;
             }
           }
         }
 
         /// main convolution loop
-        for (int kh = 0; kh < k_h; ++kh)
-        {
-          for (int kw = 0; kw < k_w; ++kw)
-          {
+        for (int kh = 0; kh < k_h; ++kh) {
+          for (int kw = 0; kw < k_w; ++kw) {
             /// preload kernel
-            for (int ic = 0; ic < in_c; ic++)
-            {
-              for (int oc = 0; oc < out_c_low; oc++)
-              {
+            for (int ic = 0; ic < in_c; ic++) {
+              for (int oc = 0; oc < out_c_low; oc++) {
                 /// currently kernel oerder is NoHWCNi, which means the
                 /// outermost dimension "N" is split into 2 high and low parts.
                 int idx_k = (kh * k_w * in_c * out_c_low) +
@@ -111,26 +93,21 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
               }
             }
 
-            for (int ih = 0; ih < p::in_tile_h; ++ih)
-            {
-              for (int iw = 0; iw < p::in_tile_w; ++iw)
-              {
+            for (int ih = 0; ih < p::in_tile_h; ++ih) {
+              for (int iw = 0; iw < p::in_tile_w; ++iw) {
                 int oh = ih - kh;
                 int ow = iw - kw;
                 bool output_on = (oh >= 0) && (ow >= 0) && (oh < p::tile_h) &&
                                  (ow < p::tile_w);
 
-                for (int ic = 0; ic < in_c; ic++)
-                {
+                for (int ic = 0; ic < in_c; ic++) {
                   T_in in_elem = in_buf[ih][iw][ic];
 
-                  for (int oc = 0; oc < out_c_low; oc++)
-                  {
+                  for (int oc = 0; oc < out_c_low; oc++) {
                     T_k k_elem = k_buf[ic][oc];
                     T_out acc_tmp = in_elem * k_elem;
 
-                    if (output_on)
-                    {
+                    if (output_on) {
                       out_buf[oh][ow][oc] += acc_tmp;
                       // printf("in_elem: %d, k_elem: %d\n",
                       //        in_elem, k_elem);
@@ -146,17 +123,13 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
         }
 
         /// export data in output buffer step
-        for (int oh = 0; oh < p::tile_h; ++oh)
-        {
-          for (int ow = 0; ow < p::tile_w; ++ow)
-          {
-            for (int oc = 0; oc < out_c_low; oc++)
-            {
+        for (int oh = 0; oh < p::tile_h; ++oh) {
+          for (int ow = 0; ow < p::tile_w; ++ow) {
+            for (int oc = 0; oc < out_c_low; oc++) {
               T_out out = out_buf[oh][ow][oc];
               T_out tmp;
 
-              if (threshold_data != NULL)
-              {
+              if (threshold_data != NULL) {
                 T_out ts0 = threshold_buf[oc][0];
                 T_out ts1 = threshold_buf[oc][1];
                 T_out ts2 = threshold_buf[oc][2];
@@ -172,8 +145,7 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
                     tmp = 2;
                   else
                     tmp = 3;
-                }
-                else if (flag == -1) /// decreasing function
+                } else if (flag == -1) /// decreasing function
                 {
                   if (out > ts2)
                     tmp = 0;
@@ -183,16 +155,12 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
                     tmp = 2;
                   else
                     tmp = 3;
-                }
-                else
-                {
+                } else {
                   /// max value of 2 bits
                   T_out k = 3 * 3 * out_c * 3;
                   tmp = flag - k;
                 }
-              }
-              else
-              {
+              } else {
                 tmp = out;
               }
 
@@ -203,8 +171,7 @@ void conv_kn2row_tiling_impl(T_in in_data[], T_out out_data[], T_k k_data[],
 
               bool output_on =
                   ((oh_ < out_h) && (ow_ < out_w) && (oc_ < out_c));
-              if (output_on)
-              {
+              if (output_on) {
                 int idx_out = oh_ * out_w * out_c + ow_ * out_c + oc_;
                 // printf("oh: %d, ow: %d, oc: %d, idx: %d, tmp: %d\n", oh_,
                 // ow_,
@@ -225,9 +192,7 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
                               const unsigned in_b, const unsigned out_w,
                               const unsigned out_h, const unsigned out_c,
                               const unsigned k_w, const unsigned k_h,
-                              const unsigned pad)
-{
-  /*
+                              const unsigned pad) {
   /// just alias for better understanding
   static const unsigned out_c_low = p::num_pe;
   assert((out_c % out_c_low) == 0);
@@ -240,25 +205,20 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
   assert(k_w <= p::max_k_w);
   assert(k_w >= p::min_k_w);
 
-  for (int oc_high = 0; oc_high < out_c; oc_high += out_c_low)
-  {
+  for (int oc_high = 0; oc_high < out_c; oc_high += out_c_low) {
     T_out threshold_buf[out_c_low][p::num_thresholds];
 
-    if (threshold_data != NULL)
-    {
-      for (unsigned oc = 0; oc < out_c_low; oc++)
-      {
-        for (unsigned i = 0; i < p::num_thresholds; i++)
-        {
-          threshold_buf[oc][i] = threshold_data[oc_high + oc + i];
+    if (threshold_data != NULL) {
+      for (unsigned oc = 0; oc < out_c_low; oc++) {
+        for (unsigned i = 0; i < p::num_thresholds; i++) {
+          unsigned idx_th = (oc_high + oc) * p::num_thresholds + i;
+          threshold_buf[oc][i] = threshold_data[idx_th];
         }
       }
     }
 
-    for (int ih_high = 0; ih_high < in_h + 2 * pad; ih_high += p::tile_h)
-    {
-      for (int iw_high = 0; iw_high < in_w + 2 * pad; iw_high += p::tile_w)
-      {
+    for (int ih_high = 0; ih_high < in_h + 2 * pad; ih_high += p::tile_h) {
+      for (int iw_high = 0; iw_high < in_w + 2 * pad; iw_high += p::tile_w) {
         T_q in_buf[p::in_tile_h][p::in_tile_w][p::max_in_c_by_word]
                   [p::max_in_b];
         T_out out_buf[p::tile_w][p::tile_w][out_c_low];
@@ -268,10 +228,8 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
         //           << std::endl;
 
         /// preload input
-        for (int ih_low = 0; ih_low < p::in_tile_h; ++ih_low)
-        {
-          for (int iw_low = 0; iw_low < p::in_tile_w; ++iw_low)
-          {
+        for (int ih_low = 0; ih_low < p::in_tile_h; ++ih_low) {
+          for (int iw_low = 0; iw_low < p::in_tile_w; ++iw_low) {
             /// index must care the padding, so we skip the padding part that
             /// doesn't exist in actuall memory.
             int ih = (ih_low + ih_high - pad);
@@ -279,13 +237,12 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
             bool input_on =
                 (ih >= 0) && (iw >= 0) && (ih < in_h) && (iw < in_w);
 
-            for (int ic = 0; ic < in_c_by_word; ic++)
-            {
-              for (int ib = 0; ib < in_b; ib++)
-              {
-                int idx_in = ih * in_w * in_c_by_word * in_b + iw * in_c_by_word
-  * in_b + ic * in_b f + ib; in_buf[ih_low][iw_low][ic][ib] = (input_on) ?
-  in_data[idx_in] : T_q(0);
+            for (int ic = 0; ic < in_c_by_word; ic++) {
+              for (int ib = 0; ib < in_b; ib++) {
+                int idx_in = ih * in_w * in_c_by_word * in_b +
+                             iw * in_c_by_word * in_b + ic * in_b + ib;
+                in_buf[ih_low][iw_low][ic][ib] =
+                    (input_on) ? in_data[idx_in] : T_q(0);
                 // printf("in_buf[%d][%d][%d] = %d\n", ih_low, iw_low, ic,
                 //        int(in_buf[ih_low][iw_low][ic]));
               }
@@ -296,58 +253,51 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
         /// initialize output_buf
         /// TODO: this could be done at the same time in the accumuratoin
         /// step.
-        for (int oh = 0; oh < p::tile_h; ++oh)
-        {
-          for (int ow = 0; ow < p::tile_w; ++ow)
-          {
-            for (int oc = 0; oc < out_c_low; ++oc)
-            {
+        for (int oh = 0; oh < p::tile_h; ++oh) {
+          for (int ow = 0; ow < p::tile_w; ++ow) {
+            for (int oc = 0; oc < out_c_low; ++oc) {
               out_buf[oh][ow][oc] = 0;
             }
           }
         }
 
         /// main convolution loop
-        for (int kh = 0; kh < k_h; ++kh)
-        {
-          for (int kw = 0; kw < k_w; ++kw)
-          {
+        for (int kh = 0; kh < k_h; ++kh) {
+          for (int kw = 0; kw < k_w; ++kw) {
             /// preload kernel
-            for (int ic = 0; ic < in_c_by_word; ic++)
-            {
-              for (int ib = 0; ib < in_b; ib++)
-              {
-                for (int oc = 0; oc < out_c_low; oc++)
-                {
+            for (int ic = 0; ic < in_c_by_word; ic++) {
+              for (int ib = 0; ib < in_b; ib++) {
+                for (int oc = 0; oc < out_c_low; oc++) {
                   /// currently kernel oerder is NoHWCNi, which means the
                   /// outermost dimension "N" is split into 2 high and low
-  parts. int idx_k = (kh * k_w * in_c_by_word * out_c_low) + (kw * in_c_by_word
-  * out_c_low) + (ic * out_c_low) + oc + (oc_high * k_h * k_w * in_c_by_word);
+                  /// parts. we should be carefull when compute the index.
+                  int idx_k = (kh * k_w * in_c_by_word * out_c_low) +
+                              (kw * in_c_by_word * out_c_low) +
+                              (ic * out_c_low) + oc +
+                              (oc_high * k_h * k_w * in_c_by_word);
                   k_buf[ic][oc] = k_data[idx_k];
                 }
               }
             }
 
-            for (int ih = 0; ih < p::in_tile_h; ++ih)
-            {
-              for (int iw = 0; iw < p::in_tile_w; ++iw)
-              {
+            for (int ih = 0; ih < p::in_tile_h; ++ih) {
+              for (int iw = 0; iw < p::in_tile_w; ++iw) {
                 int oh = ih - kh;
                 int ow = iw - kw;
                 bool output_on = (oh >= 0) && (ow >= 0) && (oh < p::tile_h) &&
                                  (ow < p::tile_w);
 
-                for (int ic = 0; ic < in_c_by_word; ic++)
-                {
-                  T_q in_elem = in_buf[ih][iw][ic];
+                for (int ic = 0; ic < in_c_by_word; ic++) {
+                  T_q in_elems[p::max_in_b];
+                  for (int ib = 0; ib < in_b; ib++) {
+                    in_elems[ib] = in_buf[ih][iw][ic][ib];
+                  }
 
-                  for (int oc = 0; oc < out_c_low; oc++)
-                  {
+                  for (int oc = 0; oc < out_c_low; oc++) {
                     T_q k_elem = k_buf[ic][oc];
-                    T_out acc_tmp = in_elem * k_elem;
+                    T_out acc_tmp = PE(k_elem, in_elems[0], in_elems[1]);
 
-                    if (output_on)
-                    {
+                    if (output_on) {
                       out_buf[oh][ow][oc] += acc_tmp;
                       // printf("in_elem: %d, k_elem: %d\n",
                       //        in_elem, k_elem);
@@ -363,17 +313,13 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
         }
 
         /// export data in output buffer step
-        for (int oh = 0; oh < p::tile_h; ++oh)
-        {
-          for (int ow = 0; ow < p::tile_w; ++ow)
-          {
-            for (int oc = 0; oc < out_c_low; oc++)
-            {
+        for (int oh = 0; oh < p::tile_h; ++oh) {
+          for (int ow = 0; ow < p::tile_w; ++ow) {
+            for (int oc = 0; oc < out_c_low; oc++) {
               T_out out = out_buf[oh][ow][oc];
               T_out tmp;
 
-              if (threshold_data != NULL)
-              {
+              if (threshold_data != NULL) {
                 T_out ts0 = threshold_buf[oc][0];
                 T_out ts1 = threshold_buf[oc][1];
                 T_out ts2 = threshold_buf[oc][2];
@@ -389,8 +335,7 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
                     tmp = 2;
                   else
                     tmp = 3;
-                }
-                else if (flag == -1) /// decreasing function
+                } else if (flag == -1) /// decreasing function
                 {
                   if (out > ts2)
                     tmp = 0;
@@ -400,16 +345,12 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
                     tmp = 2;
                   else
                     tmp = 3;
-                }
-                else
-                {
+                } else {
                   /// max value of 2 bits
                   T_out k = 3 * 3 * out_c * 3;
                   tmp = flag - k;
                 }
-              }
-              else
-              {
+              } else {
                 tmp = out;
               }
 
@@ -420,8 +361,7 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
 
               bool output_on =
                   ((oh_ < out_h) && (ow_ < out_w) && (oc_ < out_c));
-              if (output_on)
-              {
+              if (output_on) {
                 int idx_out = oh_ * out_w * out_c + ow_ * out_c + oc_;
                 // printf("oh: %d, ow: %d, oc: %d, idx: %d, tmp: %d\n", oh_,
                 // ow_,
@@ -434,7 +374,6 @@ void qconv_kn2row_tiling_impl(T_q in_data[], T_out out_data[], T_q k_data[],
       }
     }
   }
-  */
 }
 
 } // namespace cpp
