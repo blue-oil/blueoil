0283ddd1324d0996ce8cc09ee85c435f5a196e2d
lm_konda <konda@leapmind.io>
2018-11-15 18:51:49 +0900

    remove redundant lines

--- blueoil_test.sh
+++ blueoil_test.sh
@@ -215,8 +215,6 @@ if [ "${YML_CONFIG_FILE}" == "" ]; then
             do
                 TRAINING_DATASET_PATH=$(get_yaml_param train_path tests/config/${TEST_CASE}.yml)
                 VALIDATION_DATASET_PATH=$(get_yaml_param test_path tests/config/${TEST_CASE}.yml)
-                INITIAL_LEARNING_RATE=$(get_yaml_param initial_learning_rate tests/config/${TEST_CASE}.yml)
-                TRAINING_LEARNING_RATE=$(get_yaml_param training_learning_rate tests/config/${TEST_CASE}.yml)
 		init_test ${TEST_CASE} ${TASK_TYPE_NUMBER} 1 1 ${DATASET_FORMAT_NUMBER} ${TRAINING_DATASET_PATH} ${VALIDATION_DATASET_PATH} 
                 basic_test
                 if [ ${ADDITIONAL_TEST_FLAG} -eq 0 ]; then
