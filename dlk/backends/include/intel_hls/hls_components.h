c666612b0efbe622e98d2081b1dca3c9c9aef9d4
tkclimb <tkclimb0911@gmail.com>
2018-11-16 18:14:39 +0900

    Add intel HLS implementation of qconv_kn2row_tiling without any optimization.

--- dlk/backends/include/intel_hls/hls_components.h
+++ dlk/backends/include/intel_hls/hls_components.h
@@ -158,6 +158,8 @@ void intel_hls_qconv_with_kn2row(T_q in_data_packed[], T_out out_data[], T_q k_d
                                  unsigned pad, unsigned stride)
 {
   assert(((k_h == 3) && (k_w == 3)) || ((k_h == 1) && (k_w == 1)));
+  assert(((k_h == 3) && (pad == 1)) || ((k_h == 1) && (pad == 0)));
+  assert(stride == 1);
 
   const unsigned in_size = in_h * in_w * in_c_by_word * nbits_in_data;
   const unsigned out_size = out_h * out_w * out_c;
@@ -237,6 +239,8 @@ void intel_hls_a8w1_qconv_with_kn2row(T_in_k2c in_data[], T_out_k2c out_data[], 
                                       unsigned k_w, unsigned k_h, unsigned pad, unsigned stride)
 {
   assert((k_h == 3) && (k_w == 3));
+  assert(((k_h == 3) && (pad == 1)) || ((k_h == 1) && (pad == 0)));
+  assert(stride == 1);
 
   const unsigned in_size = in_h * in_w * in_c;
   const unsigned out_size = out_h * out_w * out_c;
@@ -299,11 +303,45 @@ hls_avalon_slave_component void intel_hls_qconv_kn2row_tiling_impl(
   hls_avalon_slave_register_argument
     ihc::mm_master<T_k_hls, ihc::aspace<3>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
                    ihc::align<16>, ihc::waitrequest<true> > &k_data,
-  hls_avalon_slave_register_argument
-    ihc::mm_master<T_out_hls, ihc::aspace<4>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
-                   ihc::align<16>, ihc::waitrequest<true> > &out_data_partial,
+  ihc::mm_master<T_out_hls, ihc::aspace<3>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
+                 ihc::align<16>, ihc::waitrequest<true> > &threshold_data,
   hls_avalon_slave_register_argument int32 in_w, hls_avalon_slave_register_argument int32 in_h,
   hls_avalon_slave_register_argument int32 in_c_by_word, hls_avalon_slave_register_argument int32 out_w,
   hls_avalon_slave_register_argument int32 out_h, hls_avalon_slave_register_argument int32 out_c,
   hls_avalon_slave_register_argument int32 k_w, hls_avalon_slave_register_argument int32 k_h,
   hls_avalon_slave_register_argument int32 pad, hls_avalon_slave_register_argument int32 use_threshold);
+
+void intel_hls_qconv_kn2row_tiling(T_q in_data_packed[], T_out out_data[], T_q k_data_packed[], T_out th_data[],
+                                   unsigned in_w, unsigned in_h, unsigned in_c_by_word, unsigned nbits_in_data,
+                                   unsigned out_w, unsigned out_h, unsigned out_c, unsigned k_w, unsigned k_h,
+                                   unsigned pad, unsigned stride)
+{
+  assert(((k_h == 3) && (k_w == 3)) || ((k_h == 1) && (k_w == 1)));
+  assert(((k_h == 3) && (pad == 1)) || ((k_h == 1) && (pad == 0)));
+  assert(stride == 1);
+
+  const unsigned in_size = in_h * in_w * in_c_by_word * nbits_in_data;
+  const unsigned out_size = out_h * out_w * out_c;
+  const unsigned k_size = k_h * k_w * in_c_by_word * out_c;
+  const unsigned num_th = conv_common_params::num_thresholds;
+  const int use_threshold = (th_data != NULL) ? 1 : 0;
+
+  ihc::mm_master<T_in_hls, ihc::aspace<1>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
+                 ihc::align<16>, ihc::waitrequest<true> >
+    avmm_in(in_data_packed, (in_size + in_w * in_c_by_word) * sizeof(T_in_hls));
+
+  ihc::mm_master<T_out_hls, ihc::aspace<2>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
+                 ihc::align<16>, ihc::waitrequest<true> >
+    avmm_out(out_data, out_size * sizeof(T_out_hls));
+
+  ihc::mm_master<T_k_hls, ihc::aspace<3>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
+                 ihc::align<16>, ihc::waitrequest<true> >
+    avmm_k(k_data_packed, k_size * sizeof(T_k_hls));
+
+  ihc::mm_master<T_out_hls, ihc::aspace<3>, ihc::awidth<32>, ihc::dwidth<128>, ihc::latency<0>, ihc::maxburst<32>,
+                 ihc::align<16>, ihc::waitrequest<true> >
+    avmm_th(th_data, out_c * num_th * sizeof(T_out_hls));
+
+  intel_hls_qconv_kn2row_tiling_impl(avmm_in, avmm_out, avmm_k, avmm_th, in_w, in_h, in_c_by_word, out_w, out_h, out_c,
+                                     k_h, k_w, pad, use_threshold);
+}
