4deafec52de0227ee1c8ac045777a9a1e5a68eb9
lm_konda <konda@leapmind.io>
2018-12-07 10:34:06 +0900

    support RMSPropOptimizer

--- blueoil/templates/lmnet/classification.tpl.py
+++ blueoil/templates/lmnet/classification.tpl.py
@@ -96,7 +96,9 @@ elif '{{optimizer}}' == 'ProximalAdagradOptimizer':
     NETWORK.OPTIMIZER_CLASS = tf.train.ProximalAdagradOptimizer
 elif '{{optimizer}}' == 'ProximalGradientDescentOptimizer':
     NETWORK.OPTIMIZER_CLASS = tf.train.ProximalGradientDescentOptimizer
-    
+elif '{{optimizer}}' == 'RMSPropOptimizer':
+    NETWORK.OPTIMIZER_CLASS = tf.train.RMSPropOptimizer
+
 if '{{learning_rate_setting}}' != 'fixed':
     NETWORK.OPTIMIZER_CLASS = tf.train.AdagradOptimizer    
     
