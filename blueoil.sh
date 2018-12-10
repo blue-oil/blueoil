57bd4f297af343bc684556fb37cbce90be56eb22
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2018-12-10 17:35:19 +0900

    remove dlk from -v option

--- blueoil.sh
+++ blueoil.sh
@@ -91,11 +91,11 @@ SHARED_DOCKER_OPTIONS="--rm -t -u ${USER_ID}:${GROUP_ID} ${PYHONPATHS}"
 
 # Mount source code directories if they are exist on host.
 # Docker's -v option overwrite container's directory with mounted host directory.
-if [ -e lmnet ] && [ -e dlk ]  &&  [ -e blueoil ] ; then
+# Currently, we do not mount dlk, because dlk directory include pre compiled library
+if [ -e lmnet ] && [ -e blueoil ] ; then
 	touch lmnet/lmnet/__init__.py
 	SHARED_DOCKER_OPTIONS=${SHARED_DOCKER_OPTIONS}" \
 		-v ${ABS_BASE_DIR}/lmnet:${GUEST_HOME_DIR}/lmnet \
-		-v ${ABS_BASE_DIR}/dlk:${GUEST_HOME_DIR}/dlk \
 		-v ${ABS_BASE_DIR}/blueoil:${GUEST_HOME_DIR}/blueoil"
 fi
 
