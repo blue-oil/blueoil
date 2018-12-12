9dfe36fc6d49184228cdada8beb46d19917d7630
Yoshihiro Yamazaki <yoya@awm.jp>
2018-12-12 16:51:17 +0900

    correct code comment to data format CWH => CHW

--- runtime/test/test_data_processor.cpp
+++ runtime/test/test_data_processor.cpp
@@ -59,7 +59,7 @@ float test_expect[3][4][4] =
       } };
 
 int test_data_processor_resize() {
-    // CWH (3-channel, height, width)
+    // CHW (3-channel, height, width)
     int width = 4, height = 4;
     const std::pair<int, int>& image_size = std::make_pair(width, height);
     blueoil::Tensor input({3, 8, 8}, (float *)test_input);
