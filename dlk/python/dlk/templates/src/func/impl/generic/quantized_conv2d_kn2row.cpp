97bf447d274bd1c2937c1fc32a0e720c50a2dcc1
lm_konda <konda@leapmind.io>
2018-12-14 10:29:12 +0900

    replace NULL to nullptr

--- dlk/python/dlk/templates/src/func/impl/generic/quantized_conv2d_kn2row.cpp
+++ dlk/python/dlk/templates/src/func/impl/generic/quantized_conv2d_kn2row.cpp
@@ -151,7 +151,7 @@ void QuantizedConv2DKn2Row(QUANTIZED_NOT_PACKED input[],
 
   delete[] kernel_hwoi;
 
-  if (p.thresholds != NULL) {
+  if (p.thresholds != nullptr) {
     ApplyThresholds(output_, p);
   }
 
