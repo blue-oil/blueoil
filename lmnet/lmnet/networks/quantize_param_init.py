d4dbc46018c235f821674e661def9952cda6f915
lm-jira <jira@leapmind.io>
2019-01-07 17:22:44 +0900

    edit QuantizeParamInit class document and add unit test

--- lmnet/lmnet/networks/quantize_param_init.py
+++ lmnet/lmnet/networks/quantize_param_init.py
@@ -3,10 +3,11 @@ import functools
 
 
 class QuantizeParamInit:
-    """QuantizeParamInit.
+    """QuantizeParamInit aims to make creating a quantized network class more convenient by providing two functions.
+    1) __init___ : for initializing variables necessary for quantization, such as quantization function
+    2) _quantized_variable_getter : for quantizing weight variables in convolutional layers
 
-    This QuantizeParamInit class is a mixin class for quantized network of all kinds of task
-    (classification, object detection, and segmentation).
+    This QuantizeParamInit class can be used for all kinds of task (classification, object detection, and segmentation).
     Every sub task's network class with quantized layer should extend this class.
 
     Args:
@@ -43,10 +44,7 @@ class QuantizeParamInit:
         self.custom_getter = functools.partial(self._quantized_variable_getter,
                                                weight_quantization=weight_quantization,
                                                quantize_first_convolution=quantize_first_convolution,
-                                               quantize_last_convolution=quantize_last_convolution,
-                                               first_layer_name=self.first_layer_name,
-                                               last_layer_name=self.last_layer_name,)
-
+                                               quantize_last_convolution=quantize_last_convolution,)
         if quantize_last_convolution:
             self.before_last_activation = self.activation
         else:
@@ -59,13 +57,11 @@ class QuantizeParamInit:
                                    quantize_first_convolution=None,
                                    quantize_last_convolution=None,
                                    use_histogram=True,
-                                   first_layer_name=None,
-                                   last_layer_name=None,
                                    *args,
                                    **kwargs):
-        """Get the quantized variables of all layers.
-        The quantization of the first layer and the last layer can be skipped by
-        two variables, quantize_first_convolution and quantize_last_convolution
+        """Get the quantized weight variables of convolutional layers (variables end with "kernel"). 
+        The quantization of the first and last convolutional layers can be skipped by two variables, 
+        quantize_first_convolution and quantize_last_convolution, respectively.
         Args:
             getter: Default from tensorflow.
             name: Default from tensorflow.
@@ -73,10 +69,11 @@ class QuantizeParamInit:
             quantize_first_convolution (boolean): True if using quantization at the first layer, False otherwise
             quantize_last_convolution (boolean): True if using quantization at the last layer, False otherwise
             use_histogram (boolean): True to summarize tf.summary.histogram of quantized var before return
-            first_layer_name (string): name of the first layer's variable_scope
-            last_layer_name (string): name of the last layer's variable_scope
             args: Args.
             kwargs: Kwargs.
+
+        If quantize_first_convolution is defined, first_layer_name class variable must be defined in sub task's init function.
+        If quantize_last_convolution is defined, last_layer_name class variable must be defined in sub task's init function.
         """
         assert callable(weight_quantization)
         var = getter(name, *args, **kwargs)
@@ -85,11 +82,13 @@ class QuantizeParamInit:
             # Apply weight quantize to variable whose last word of name is "kernel".
             if "kernel" == var.op.name.split("/")[-1]:
                 if quantize_first_convolution is not None and not quantize_first_convolution:
-                    if first_layer_name is not None and var.op.name.startswith(first_layer_name):
+                    assert self.first_layer_name is not None
+                    if var.op.name.startswith(self.first_layer_name):
                         return var
 
                 if quantize_last_convolution is not None and not quantize_last_convolution:
-                    if last_layer_name is not None and var.op.name.startswith(last_layer_name):
+                    assert self.last_layer_name is not None
+                    if var.op.name.startswith(self.last_layer_name):
                         return var
 
                 quantized_kernel = weight_quantization(var)
