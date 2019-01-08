975ba6588fa3e2f628c135527f67fd9736cc7ea3
konda <konda@leapmind.io>
2019-01-08 16:16:44 +0900

    fix to pass test

--- dlk/python/dlk/core/view.py
+++ dlk/python/dlk/core/view.py
@@ -119,11 +119,11 @@ class View(object):
                     for k, v in input_ops['X'].output_ops.items():
                         if v[0] == op:
                             inputs_string = str(input_ops['X'].name) + '_' + str(k)
-                    in_str_t = inputs_string + ', ' + input_ops['W'].name + '_transposed'
+                    istrt = inputs_string + ', ' + input_ops['W'].name + '_transposed'
                     inputs_string = inputs_string + ', ' + input_ops['W'].name
                 else:
                     iops = input_ops
-                    in_str_t = ', '.join(str(x.name) if k!='W' else str(x.name) + '_transposed' for k,x in iops.items())
+                    istrt = ', '.join(str(x.name) if k != 'W' else str(x.name) + '_transposed' for k, x in iops.items())
                     inputs_string = self.inputs_to_string(input_ops)
 
                 if op.has_thresholds:
@@ -166,7 +166,7 @@ class View(object):
                     binConv2D_struct.max_value = {max_value};
 
                     #if defined RUN_ON_FPGA
-                    {conv_func}({in_str_t}, {op.name}, scaling_factors::{op.name}, binConv2D_struct);
+                    {conv_func}({istrt}, {op.name}, scaling_factors::{op.name}, binConv2D_struct);
                     #else
                     {conv_func}({inputs_string}, {op.name}, scaling_factors::{op.name}, binConv2D_struct);
                     #endif
