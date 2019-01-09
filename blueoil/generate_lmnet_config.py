e202a532ed239476a6cbd64b806347c64ee54b87
lm_konda <konda@leapmind.io>
2019-01-10 00:18:25 +0900

    modify config generator

--- blueoil/generate_lmnet_config.py
+++ blueoil/generate_lmnet_config.py
@@ -155,10 +155,10 @@ def _blueoil_to_lmnet(blueoil_config):
 
     learning_rate_func = None
     learning_rate_kwargs = None
-    if learning_rate_schedule == "constant":
+    if optimizer == "MomentumOptimizer":
         optimizer_kwargs = {"momentum": 0.9, "learning_rate": initial_learning_rate}
     else:
-        optimizer_kwargs = {"momentum": 0.9}
+        optimizer_kwargs = {"learning_rate": initial_learning_rate}
         learning_rate_func = "tf.train.piecewise_constant"
 
     if learning_rate_schedule == "2-step-decay":
