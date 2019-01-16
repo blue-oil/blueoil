98ea890c7ab44111f69d285f208f97278089b955
nlpng <neil.phuang@gmail.com>
2019-01-17 08:49:45 +0900

    just add more tests for more optimizatino pass

--- dlk/tests/test_optimizer.py
+++ dlk/tests/test_optimizer.py
@@ -21,7 +21,8 @@ from core.optimizer import pass_remove_identities, pass_transpose, pass_constant
     pass_quantize_convolutions, pass_propagate_datatypes, pass_propagate_output_type_backward
 from core.graph import Graph
 from core.operators import Add, AveragePool, BatchNormalization, Constant, Conv, Identity, Input, \
-    MaxPool, Operator, Output, Transpose, QTZ_binary_mean_scaling, QTZ_linear_mid_tread_half, Reshape, Softmax
+    MaxPool, Operator, Output, Transpose, QTZ_binary_mean_scaling, QTZ_linear_mid_tread_half, Reshape, Softmax, \
+    SpaceToDepth
 
 import numpy as np
 from typing import Tuple
@@ -240,6 +241,177 @@ class TestPassPropagateQuantizationDetailsIntoConv(unittest.TestCase):
 
         # One output
         y = Output('output', [1, 3, 3, 3], Float32(), {'input': conv2})
+
+        # add ops to the graph
+        graph.add_op_and_inputs(y)
+
+        return graph
+
+
+class TestPassPackWeights(unittest.TestCase):
+    """Test class for packing weight."""
+    def test_pass_pack_weights(self) -> None:
+        """Test pass."""
+        data1 = np.float32(np.random.rand(1, 2, 2, 3))
+        data2 = np.float32(np.random.rand(1, 2, 2, 3))
+        graph1 = self.create_sample_graph(data1, data2)
+
+        pass_pack_weights(graph1)
+
+        self.assertEqual(graph1.get_op('conv2').input_ops['W'].op_type, 'Constant',
+                         '[Failed] Found input kernel weights not a constant')
+
+        print("Test pack_weights #4 pass passed!")
+
+    @staticmethod
+    def create_sample_graph(data1: np.ndarray, data2: np.ndarray) -> Graph:
+        graph = Graph()
+
+        # input
+        x = Input('placeholder', [1, 5, 5, 3], Float32())
+
+        # Conv1
+        w1 = Constant('weight1', Float32(), data1)
+        conv1 = Conv('conv1', [1, 4, 4, 3], Float32(), {'X': x, 'W': w1}, kernel_shape=[2, 2])
+
+        # activation quantizer
+        s1 = Constant('aq_const1', Float32(), np.array(1))
+        s2 = Constant('aq_const2', Float32(), np.array(2))
+        aq = QTZ_linear_mid_tread_half('aqtz1', [1, 4, 4, 3], Float32(), {'X': conv1, 'Y': s1, 'Z': s2})
+
+        # Conv2
+        w2 = Constant('weight2', Float32(), data2)
+        kq = QTZ_binary_mean_scaling('kqtz1', [1, 2, 2, 3], Float32(), {'input': w2})
+        conv2 = Conv('conv2', [1, 3, 3, 3], Float32(), {'X': aq, 'W': kq}, kernel_shape=[2, 2])
+        conv2.a_quantizer = [aq]
+        conv2.quantizer = kq
+
+        # One output
+        y = Output('output', [1, 3, 3, 3], Float32(), {'input': conv2})
+
+        # add ops to the graph
+        graph.add_op_and_inputs(y)
+
+        return graph
+
+
+class TestPassQuantizeConvolutions(unittest.TestCase):
+    """Test class for packing weight."""
+    def test_pass_quantize_convolutions(self) -> None:
+        """Test pass."""
+        data1 = np.float32(np.random.rand(1, 2, 2, 3))
+        data2 = np.float32(np.random.rand(1, 2, 2, 3))
+        graph1 = self.create_sample_graph(data1, data2)
+
+        pass_quantize_convolutions(graph1)
+
+        self.assertEqual(graph1.get_op('aqtz1').dtype, QUANTIZED_NOT_PACKED(),
+                         '[Failed] Found output dtype of activation quantizer not proper')
+        self.assertEqual(graph1.get_op('conv2').dtype, Float32(),
+                         '[Failed] Found output dtype of conv not proper')
+
+        print("Test quantize_convolutions #5 pass passed!")
+
+    @staticmethod
+    def create_sample_graph(data1: np.ndarray, data2: np.ndarray) -> Graph:
+        graph = Graph()
+
+        # input
+        x = Input('placeholder', [1, 5, 5, 3], Float32())
+
+        # Conv1
+        w1 = Constant('weight1', Float32(), data1)
+        conv1 = Conv('conv1', [1, 4, 4, 3], Float32(), {'X': x, 'W': w1}, kernel_shape=[2, 2])
+
+        # activation quantizer
+        s1 = Constant('aq_const1', Float32(), np.array(1))
+        s2 = Constant('aq_const2', Float32(), np.array(2))
+        aq = QTZ_linear_mid_tread_half('aqtz1', [1, 4, 4, 3], Float32(), {'X': conv1, 'Y': s1, 'Z': s2})
+
+        # Conv2
+        w2 = Constant('weight2', Float32(), data2)
+        kq = QTZ_binary_mean_scaling('kqtz1', [1, 2, 2, 3], Float32(), {'input': w2})
+        conv2 = Conv('conv2', [1, 3, 3, 3], Float32(), {'X': aq, 'W': kq}, kernel_shape=[2, 2])
+        conv2.a_quantizer = [aq]
+        conv2.quantizer = kq
+
+        # One output
+        y = Output('output', [1, 3, 3, 3], Float32(), {'input': conv2})
+
+        # add ops to the graph
+        graph.add_op_and_inputs(y)
+
+        return graph
+
+
+class TestPassPropagateDatatypes(unittest.TestCase):
+    """Test class for packing weight."""
+    def test_pass_propagate_datatypes(self) -> None:
+        """Test pass."""
+        data1 = np.float32(np.random.rand(1, 2, 2, 3))
+        graph1 = self.create_sample_graph(data1)
+        # graph2 = self.create_expected_graph(data1, data2)
+
+        pass_propagate_datatypes(graph1)
+
+        self.assertEqual(graph1.get_op('s2d').dtype, QUANTIZED_NOT_PACKED(),
+                         '[Failed] Found dtype of SpaceToDepth not propagate correctly')
+
+        print("Test propagate datatypes #6 pass passed!")
+
+    @staticmethod
+    def create_sample_graph(data1: np.ndarray) -> Graph:
+        graph = Graph()
+
+        # input
+        x = Input('placeholder', [1, 5, 5, 3], Float32())
+
+        # Conv1
+        w1 = Constant('weight1', Float32(), data1)
+        conv1 = Conv('conv1', [1, 4, 4, 3], QUANTIZED_NOT_PACKED(), {'X': x, 'W': w1}, kernel_shape=[2, 2])
+
+        pool1 = SpaceToDepth('s2d', [1, 2, 2, 12], Float32(), {'input': conv1})
+
+        # One output
+        y = Output('output', [1, 2, 2, 12], Float32(), {'input': pool1})
+
+        # add ops to the graph
+        graph.add_op_and_inputs(y)
+
+        return graph
+
+
+class TestPassPropagateOutputTypeBackward(unittest.TestCase):
+    """Test class for packing weight."""
+    def test_pass_propagate_output_type_backward(self) -> None:
+        """Test pass."""
+        data1 = np.float32(np.random.rand(1, 2, 2, 3))
+        graph1 = self.create_sample_graph(data1)
+        # graph2 = self.create_expected_graph(data1, data2)
+
+        pass_propagate_output_type_backward(graph1)
+
+        self.assertEqual(graph1.get_op('conv1').dtype, Float32(),
+                         '[Failed] Found dtype of SpaceToDepth not propagate correctly')
+
+        print("Test propagate output type backward #7 pass passed!")
+
+    @staticmethod
+    def create_sample_graph(data1: np.ndarray) -> Graph:
+        graph = Graph()
+
+        # input
+        x = Input('placeholder', [1, 5, 5, 3], Float32())
+
+        # Conv1
+        w1 = Constant('weight1', Float32(), data1)
+        conv1 = Conv('conv1', [1, 4, 4, 3], QUANTIZED_NOT_PACKED(), {'X': x, 'W': w1}, kernel_shape=[2, 2])
+        conv1.is_quantized = True
+
+        pool1 = SpaceToDepth('s2d', [1, 2, 2, 12], Float32(), {'input': conv1})
+
+        # One output
+        y = Output('output', [1, 2, 2, 12], Float32(), {'input': pool1})
 
         # add ops to the graph
         graph.add_op_and_inputs(y)
