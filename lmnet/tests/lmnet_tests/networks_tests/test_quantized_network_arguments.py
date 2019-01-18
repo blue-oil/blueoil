2f9eed7f92ac0d64f9d5383a26191749f3781797
lm-jira <jira@leapmind.io>
2019-01-18 15:40:30 +0900

    add LmnetQuantize unit test

---
+++ lmnet/tests/lmnet_tests/networks_tests/test_quantized_network_arguments.py
@@ -0,0 +1,11 @@
+from lmnet.networks.classification.lmnet_quantize import LmnetQuantize
+
+def test_required_arguments():
+    quantizer = LmnetQuantize()
+
+    assert quantizer.first_layer_name is not None
+    assert quantizer.last_layer_name is not None
+
+
+if __name__ == '__main__':
+    test_required_arguments()
