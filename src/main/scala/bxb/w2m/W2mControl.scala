9d9775046ecb33e0dc340d3145036dca8facdd03
Nikolay Nez <nikolay.nez@gmail.com>
2019-01-18 12:36:38 +0900

    add w2m: W2mPipeline

--- src/main/scala/bxb/w2m/W2mControl.scala
+++ src/main/scala/bxb/w2m/W2mControl.scala
@@ -3,8 +3,13 @@ package bxb.w2m
 import chisel3._
 
 class W2mControl(private val wAddrWidth: Int) extends Bundle {
+  // WMem control
   val wAddr = UInt(wAddrWidth.W)
+  // M control
   val mWe = Vec(2, Bool())
+  // Sync control
+  val mRawInc = Bool()
+  val wWarInc = Bool()
 }
 
 object W2mControl {
