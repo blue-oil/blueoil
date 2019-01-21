0da93c3cd87ed0a85a2956459e79d09e1d63b622
lm-jira <jira@leapmind.io>
2019-01-21 11:07:43 +0900

    reformat code

--- lmnet/tests/lmnet_tests/networks_tests/test_quantized_network_arguments.py
+++ lmnet/tests/lmnet_tests/networks_tests/test_quantized_network_arguments.py
@@ -20,33 +20,36 @@ from lmnet.quantizations import (
 
 
 def test_required_arguments():
-    model_classes = [LmnetV1Quantize,
-                     LmnetV0Quantize,
-                     DarknetQuantize,
-                     LMFYoloQuantize,
-                     YoloV2Quantize,
-                     LmSegnetV0Quantize,
-                     LmSegnetV1Quantize,
-                    ]
+    model_classes = [
+        LmnetV1Quantize,
+        LmnetV0Quantize,
+        DarknetQuantize,
+        LMFYoloQuantize,
+        YoloV2Quantize,
+        LmSegnetV0Quantize,
+        LmSegnetV1Quantize,
+    ]
 
-    network_kwargs = {'ACTIVATION_QUANTIZER': linear_mid_tread_half_quantizer,
-                      'ACTIVATION_QUANTIZER_KWARGS': {'bit': 2, 'max_value': 2},
-                      'BATCH_SIZE': 10,
-                      'DATA_FORMAT': 'NHWC',
-                      'IMAGE_SIZE': [128, 128],
-                      'OPTIMIZER_CLASS': tf.train.AdamOptimizer,
-                      'OPTIMIZER_KWARGS': {'momentum': 0.9},
-                      'QUANTIZE_FIRST_CONVOLUTION': False,
-                      'WEIGHT_DECAY_RATE': 0.0005,
-                       'WEIGHT_QUANTIZER': binary_mean_scaling_quantizer,
-                      'WEIGHT_QUANTIZER_KWARGS': {},
-                     }
+    network_kwargs = {
+        'ACTIVATION_QUANTIZER': linear_mid_tread_half_quantizer,
+        'ACTIVATION_QUANTIZER_KWARGS': {'bit': 2, 'max_value': 2},
+        'BATCH_SIZE': 10,
+        'DATA_FORMAT': 'NHWC',
+        'IMAGE_SIZE': [128, 128],
+        'OPTIMIZER_CLASS': tf.train.AdamOptimizer,
+        'OPTIMIZER_KWARGS': {'momentum': 0.9},
+        'QUANTIZE_FIRST_CONVOLUTION': False,
+        'WEIGHT_DECAY_RATE': 0.0005,
+        'WEIGHT_QUANTIZER': binary_mean_scaling_quantizer,
+        'WEIGHT_QUANTIZER_KWARGS': {},
+    }
 
     for model in model_classes:
-        quantizer = model(['accordion', 'airplanes', 'anchor'],
-                          True,
-                          network_kwargs
-                         )
+        quantizer = model(
+            ['accordion', 'airplanes', 'anchor'],
+            True,
+            network_kwargs
+        )
 
         assert quantizer.first_layer_name is not None
         assert quantizer.last_layer_name is not None
