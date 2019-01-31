4e784aed012da0757ffa32823a6e1fe56f5232a1
lm-jira <jira@leapmind.io>
2019-01-31 14:16:27 +0900

    change input of base function

--- lmnet/tests/lmnet_tests/networks_tests/test_quantize_param_init.py
+++ lmnet/tests/lmnet_tests/networks_tests/test_quantize_param_init.py
@@ -67,7 +67,7 @@ def test_quantized_layers():
             weight_quantizer=binary_mean_scaling_quantizer,
         )
 
-        base, graph = quantizer.base(tf.zeros([1, 32, 32, 1]), True)
+        base, graph = quantizer.base(tf.ones([1, 32, 32, 1]), True)
         # get name of all operations using kernel and check that those operations are quantized
         op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
         assert all(quantizer_name in op_name for op_name in op_name_list)
@@ -85,7 +85,7 @@ def test_quantized_layers():
             weight_quantizer=binary_mean_scaling_quantizer,
         )
 
-        base, graph = quantizer.base(tf.zeros([1, 32, 32, 1]), True)
+        base, graph = quantizer.base(tf.ones([1, 32, 32, 1]), True)
         op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
         assert not any(quantizer.last_layer_name in op_name and quantizer_name in op_name for op_name in op_name_list)
         op_name_list = [op_name for op_name in op_name_list if quantizer.last_layer_name not in op_name]
@@ -104,7 +104,7 @@ def test_quantized_layers():
             weight_quantizer=binary_mean_scaling_quantizer,
         )
 
-        base, graph = quantizer.base(tf.zeros([1, 32, 32, 1]), True)
+        base, graph = quantizer.base(tf.ones([1, 32, 32, 1]), True)
         op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
         assert not any(quantizer.first_layer_name in op_name and quantizer_name in op_name for op_name in op_name_list)
         op_name_list = [op_name for op_name in op_name_list if quantizer.first_layer_name not in op_name]
