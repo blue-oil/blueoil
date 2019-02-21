e00b40aa9a8fd359b12ea6aed6c5d4b9fee93f36
lm_konda <konda@leapmind.io>
2019-02-21 16:03:50 +0900

    modify constant template

--- dlk/python/dlk/templates/manual/consts/input.tpl.cpp
+++ dlk/python/dlk/templates/manual/consts/input.tpl.cpp
@@ -22,14 +22,6 @@ limitations under the License.
 
 {% else -%}
 
-{% if node.transformed_data %}
-{{ node.dtype.cpptype() }} {{ node.name }}[] = {
-  {% for d in node.transposed_data -%}
-  {{- d -}},
-  {%- endfor %}
-};
-{% endif %}
-
 {% if node.transposed_data %}
 
 #if defined(RUN_ON_FPGA)
