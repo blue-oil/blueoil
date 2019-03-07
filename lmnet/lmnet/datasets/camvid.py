38f19093dc10d53a3e1fdf0391f929ece4ff5272
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2019-03-07 16:53:35 +0900

    Enable Ignore label

--- lmnet/lmnet/datasets/camvid.py
+++ lmnet/lmnet/datasets/camvid.py
@@ -22,17 +22,28 @@ from PIL import Image
 
 from lmnet.datasets.base import SegmentationBase, StoragePathCustomizable
 from lmnet.utils.random import shuffle, train_test_split
+from lmnet.common import COLOR_MAP
 
 
-def get_image(filename, convert_rgb=True):
+def get_image(filename, convert_rgb=True, ignore_class_idx=None):
     """Returns numpy array of an image"""
     image = Image.open(filename)
-
     #  sometime image data is gray.
     if convert_rgb:
         image = image.convert("RGB")
     else:
         image = image.convert("L")
+
+        # For annotation image, 'Ignore' labelled class should be removed
+        if ignore_class_idx is not None:
+            for x in range(image.size[0]):
+                for y in range(image.size[1]):
+                    pixel_value = image.getpixel((x, y))
+                    if pixel_value > ignore_class_idx:
+                        image.putpixel((x, y), pixel_value - 1)
+                    if pixel_value == ignore_class_idx:
+                        # To ignore, replace the value with enough large number
+                        image.putpixel((x, y), 255)
 
     image = np.array(image)
     return image
@@ -59,6 +70,7 @@ class CamvidBase(SegmentationBase):
         )
 
     extend_dir = "CamVid"
+    ignore_class_idx = None
 
     @property
     def available_subsets(self):
@@ -99,7 +111,7 @@ class CamvidBase(SegmentationBase):
         image_files, label_files = self.files_and_annotations
 
         image = get_image(image_files[i])
-        label = get_image(label_files[i], convert_rgb=False).copy()
+        label = get_image(label_files[i], convert_rgb=False, ignore_class_idx=self.ignore_class_idx).copy()
 
         return (image, label)
 
@@ -221,36 +233,33 @@ class CamvidCustom(StoragePathCustomizable, CamvidBase):
             *args,
             **kwargs,
         )
+        self.parse_labels()
 
     @property
     def label_colors(self):
-        colors, _ = self.parse_label_colors()
-        return np.array(colors)
+        classes = self.parse_labels()
+        return COLOR_MAP[:len(classes)]
 
     @property
     def classes(self):
-        _, classes = self.parse_label_colors()
+        classes = self.parse_labels()
         return classes
 
     @property
     def num_classes(self):
-        _, classes = self.parse_label_colors()
+        classes = self.parse_labels()
         return len(classes)
 
-    def parse_label_colors(self):
-        with open(os.path.join(self.data_dir, "label_display_colors.txt")) as f:
-            lines = f.readlines()
-            lines = [line.rstrip('\n').split('\t') for line in lines]
+    def parse_labels(self):
+        with open(os.path.join(self.data_dir, "labels.txt")) as f:
+            classes = f.readlines()
+            classes = [cls.replace('\n', '') for cls in classes]
 
-            # Split "R G B" -> ["R", "G", "B"]
-            colors = [line[0].split(' ') for line in lines]
+        if 'Ignore' in classes:
+            self.ignore_class_idx = classes.index("Ignore")
+            classes.remove("Ignore")
 
-            # TODO: Current implementation require Ignore class
-            #       is the last line of label_display_colors.txt.
-            #       Make Ignore class can exist in arbitrary line.
-            classes = [line[-1] for line in lines if line[-1] != "Ignore"]
-
-            return colors, classes
+        return classes
 
     @property
     @functools.lru_cache(maxsize=None)
--- lmnet/tests/lmnet_tests/datasets_tests/test_camvid.py
+++ lmnet/tests/lmnet_tests/datasets_tests/test_camvid.py
@@ -15,7 +15,8 @@
 # =============================================================================
 import pytest
 import numpy as np
-from lmnet.datasets.camvid import Camvid, CamvidCustom
+from PIL import Image
+from lmnet.datasets.camvid import Camvid, CamvidCustom, get_image
 from lmnet.datasets.dataset_iterator import DatasetIterator
 
 # Apply set_test_environment() in conftest.py to all tests in this file.
@@ -39,6 +40,22 @@ class DummyCamvidCustomWithoutIgnoreClass(CamvidCustom):
     extend_dir = "camvid_custom_without_ignore"
 
 
+def _ignore_label_validator(raw_image_path, loaded_image, ignore_class_id):
+    raw_image = Image.open(raw_image_path).convert("L")
+    loaded_image = Image.fromarray(loaded_image)
+    for x in range(raw_image.size[0]):
+        for y in range(raw_image.size[1]):
+            raw_pixel_value = raw_image.getpixel((x, y))
+            loaded_pixel_value = loaded_image.getpixel((x, y))
+            if raw_pixel_value > ignore_class_id:
+                if loaded_pixel_value != raw_pixel_value - 1:
+                    return False
+            elif raw_pixel_value == ignore_class_id:
+                if loaded_pixel_value != 255:
+                    return False
+    return True
+
+
 def _show_image_with_annotation(image, label, colors):
     """show image and annotation for debug"""
 
@@ -58,8 +75,8 @@ def _show_image_with_annotation(image, label, colors):
     label.show()
 
 
-def _test_camvid_basics(train_dataset, test_dataset):
-    # test training dataset
+def _test_camvid_basic(train_dataset, test_dataset):
+    # test train dataset
     train_image_files, train_label_files = train_dataset.feed()
     assert train_image_files.shape[0] == 1
     assert train_label_files.shape[0] == 1
@@ -68,7 +85,6 @@ def _test_camvid_basics(train_dataset, test_dataset):
     assert isinstance(train_images, np.ndarray)
     assert train_images.shape == (1, 360, 480, 3)
     assert train_labels.shape == (1, 360, 480)
-
     # _show_image_with_annotation(train_images[0], train_labels[0], colors)
 
     # test test dataset
@@ -80,13 +96,13 @@ def _test_camvid_basics(train_dataset, test_dataset):
     assert isinstance(test_images, np.ndarray)
     assert test_images.shape == (1, 360, 480, 3)
     assert test_labels.shape == (1, 360, 480)
-
     # _show_image_with_annotation(test_images[0], test_labels[0], colors)
 
 
 def test_camvid():
     batch_size = 1
     train_dataset = DummyCamvid(subset="train", batch_size=batch_size)
+    assert train_dataset.ignore_class_idx is None
     train_dataset = DatasetIterator(train_dataset)
     test_dataset = DummyCamvid(subset="validation", batch_size=batch_size)
     test_dataset = DatasetIterator(test_dataset)
@@ -95,21 +111,25 @@ def test_camvid():
     colors = train_dataset.label_colors
     assert len(colors) == 12
 
-    _test_camvid_basics(train_dataset, test_dataset)
+    train_image_files, train_label_files = train_dataset.feed()
+    assert train_image_files.shape[0] == 1
+    assert train_label_files.shape[0] == 1
+
+    _test_camvid_basic(train_dataset, test_dataset)
 
 
 def test_camvid_custom():
     batch_size = 1
     train_dataset = DummyCamvidCustom(subset="train", batch_size=batch_size)
+    assert train_dataset.ignore_class_idx == 3
     train_dataset = DatasetIterator(train_dataset)
     test_dataset = DummyCamvidCustom(subset="validation", batch_size=batch_size)
     test_dataset = DatasetIterator(test_dataset)
 
-    assert train_dataset.num_classes == 11
+    assert train_dataset.num_classes == 10
     colors = train_dataset.label_colors
-    assert len(colors) == 12
-
-    _test_camvid_basics(train_dataset, test_dataset)
+    assert len(colors) == 10
+    _test_camvid_basic(train_dataset, test_dataset)
 
 
 def test_camvid_custom_without_test_dataset():
@@ -119,7 +139,6 @@ def test_camvid_custom_without_test_dataset():
     train_dataset = DummyCamvidCustomWithoutTestDataset(subset="train", batch_size=batch_size,
                                                         validation_size=validation_size)
     train_dataset = DatasetIterator(train_dataset)
-
     test_dataset = DummyCamvidCustomWithoutTestDataset(subset="validation", batch_size=batch_size,
                                                        validation_size=validation_size)
     test_dataset = DatasetIterator(test_dataset)
@@ -127,20 +146,19 @@ def test_camvid_custom_without_test_dataset():
     assert train_dataset.num_per_epoch == 5 * (1 - validation_size)
     assert test_dataset.num_per_epoch == 5 * (validation_size)
 
-    image_files, label_files = train_dataset.feed()
-    assert image_files.shape[0] == 5
-    assert label_files.shape[0] == 5
-
-    image_files, label_files = test_dataset.feed()
-    assert image_files.shape[0] == 5
-    assert label_files.shape[0] == 5
-
 
 def test_camvid_custom_without_ignore():
     batch_size = 1
     train_dataset = DummyCamvidCustomWithoutIgnoreClass(subset="train", batch_size=batch_size)
-    train_dataset = DatasetIterator(train_dataset)
+    assert train_dataset.ignore_class_idx is None
 
+    train_dataset = DatasetIterator(train_dataset)
     assert train_dataset.num_classes == 12
     colors = train_dataset.label_colors
     assert len(colors) == 12
+
+
+def test_get_image():
+    path = "tests/fixtures/datasets/camvid_custom/trainannot/0001TP_006690.png"
+    loaded_image = get_image(path, convert_rgb=False, ignore_class_idx=3)
+    assert _ignore_label_validator(path, loaded_image, 3)
