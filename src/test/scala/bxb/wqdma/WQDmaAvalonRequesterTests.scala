7477f6efd3846806c90fdb701b8aeffc8a5e3dea
Nikolay Nez <nikolay.nez@gmail.com>
2019-03-13 18:05:34 +0900

    refactor WDma: generalize WDmaAvalonRequester for arbitrary element width and count

--- src/test/scala/bxb/wqdma/WQDmaAvalonRequesterTests.scala
+++ src/test/scala/bxb/wqdma/WQDmaAvalonRequesterTests.scala
@@ -1,16 +1,16 @@
-package bxb.wdma
+package bxb.wqdma
 
 import chisel3._
 import chisel3.iotesters.{PeekPokeTester, Driver}
 
-class WDmaAvalonRequesterTestSequence(dut: WDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
+class WQDmaAvalonRequesterTestSequence(dut: WQDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
   poke(dut.io.start, true)
   poke(dut.io.startAddress, 0)
   poke(dut.io.outputHCount, hCount)
   poke(dut.io.outputWCount, wCount)
-  poke(dut.io.kernelBlockCount, blockCount)
+  poke(dut.io.blockCount, blockCount)
   poke(dut.io.writerDone, true)
-  poke(dut.io.wWarZero, false)
+  poke(dut.io.warZero, false)
   poke(dut.io.avalonMasterWaitRequest, false)
   for (h <- 0 until hCount) {
     for (w <- 0 until wCount) {
@@ -27,14 +27,14 @@ class WDmaAvalonRequesterTestSequence(dut: WDmaAvalonRequester, b: Int, hCount: 
   }
 }
 
-class WDmaAvalonRequesterTestWriterDone(dut: WDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
+class WQDmaAvalonRequesterTestWriterDone(dut: WQDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
   poke(dut.io.start, true)
   poke(dut.io.startAddress, 0)
   poke(dut.io.outputHCount, hCount)
   poke(dut.io.outputWCount, wCount)
-  poke(dut.io.kernelBlockCount, blockCount)
+  poke(dut.io.blockCount, blockCount)
   poke(dut.io.writerDone, false)
-  poke(dut.io.wWarZero, false)
+  poke(dut.io.warZero, false)
   poke(dut.io.avalonMasterWaitRequest, false)
   var writerDelay = 1
   for (h <- 0 until hCount) {
@@ -59,25 +59,25 @@ class WDmaAvalonRequesterTestWriterDone(dut: WDmaAvalonRequester, b: Int, hCount
   }
 }
 
-class WDmaAvalonRequesterTestWarZero(dut: WDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
+class WQDmaAvalonRequesterTestWarZero(dut: WQDmaAvalonRequester, b: Int, hCount: Int, wCount: Int, blockCount: Int) extends PeekPokeTester(dut) {
   poke(dut.io.start, true)
   poke(dut.io.startAddress, 0)
   poke(dut.io.outputHCount, hCount)
   poke(dut.io.outputWCount, wCount)
-  poke(dut.io.kernelBlockCount, blockCount)
+  poke(dut.io.blockCount, blockCount)
   poke(dut.io.writerDone, true)
-  poke(dut.io.wWarZero, true)
+  poke(dut.io.warZero, true)
   poke(dut.io.avalonMasterWaitRequest, false)
   var warDelay = 1
   for (h <- 0 until hCount) {
     for (w <- 0 until wCount) {
       for (block <- 0 until blockCount) {
-        poke(dut.io.wWarZero, true)
+        poke(dut.io.warZero, true)
         for (_ <- 0 until warDelay) {
           step(1)
         }
         warDelay = (warDelay + 1) % b
-        poke(dut.io.wWarZero, false)
+        poke(dut.io.warZero, false)
         step(1)
         while (peek(dut.io.avalonMasterRead) == 0) {
           step(1)
@@ -91,7 +91,7 @@ class WDmaAvalonRequesterTestWarZero(dut: WDmaAvalonRequester, b: Int, hCount: I
   }
 }
 
-object WDmaAvalonRequesterTests {
+object WQDmaAvalonRequesterTests {
   def main(args: Array[String]): Unit = {
     val b = 32
     val avalonAddrWidth = 32
@@ -104,12 +104,16 @@ object WDmaAvalonRequesterTests {
     val verilatorArgs = Array("--backend-name", "verilator", "--is-verbose", "true")
     val driverArgs = if (args.contains("verilator")) verilatorArgs else Array[String]()
 
+    // wdma parameters:
+    val elementWidth = b * 1
+    val elementsPerBlock = b
+
     var ok = true
-    ok &= Driver.execute(driverArgs, () => new WDmaAvalonRequester(b, avalonAddrWidth, avalonDataWidth))(
-      dut => new WDmaAvalonRequesterTestSequence(dut, b, hCount, wCount, blockCount))
-    ok &= Driver.execute(driverArgs, () => new WDmaAvalonRequester(b, avalonAddrWidth, avalonDataWidth))(
-      dut => new WDmaAvalonRequesterTestWriterDone(dut, b, hCount, wCount, blockCount))
-    ok &= Driver.execute(driverArgs, () => new WDmaAvalonRequester(b, avalonAddrWidth, avalonDataWidth))(
-      dut => new WDmaAvalonRequesterTestWarZero(dut, b, hCount, wCount, blockCount))
+    ok &= Driver.execute(driverArgs, () => new WQDmaAvalonRequester(avalonAddrWidth, avalonDataWidth, elementWidth, elementsPerBlock))(
+      dut => new WQDmaAvalonRequesterTestSequence(dut, b, hCount, wCount, blockCount))
+    ok &= Driver.execute(driverArgs, () => new WQDmaAvalonRequester(avalonAddrWidth, avalonDataWidth, elementWidth, elementsPerBlock))(
+      dut => new WQDmaAvalonRequesterTestWriterDone(dut, b, hCount, wCount, blockCount))
+    ok &= Driver.execute(driverArgs, () => new WQDmaAvalonRequester(avalonAddrWidth, avalonDataWidth, elementWidth, elementsPerBlock))(
+      dut => new WQDmaAvalonRequesterTestWarZero(dut, b, hCount, wCount, blockCount))
   }
 }
