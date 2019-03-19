a6490aa7d43072c1ce76c0f0abb1b72c723b058d
lm-jira <jira@leapmind.io>
2019-03-19 14:41:01 +0900

    separate test case

--- lmnet/tests/lmnet_tests/networks_tests/test_quantize_param_init.py
+++ lmnet/tests/lmnet_tests/networks_tests/test_quantize_param_init.py
@@ -1,3 +1,4 @@
+import pytest
 import tensorflow as tf
 
 from lmnet.networks.classification.lmnet_v0 import LmnetV0Quantize
@@ -5,85 +6,287 @@ from lmnet.networks.classification.lmnet_v1 import LmnetV1Quantize
 from lmnet.networks.classification.darknet import DarknetQuantize
 from lmnet.networks.object_detection.lm_fyolo import LMFYoloQuantize
 from lmnet.networks.object_detection.yolo_v2_quantize import YoloV2Quantize
+
 from lmnet.quantizations import (
     binary_mean_scaling_quantizer,
     linear_mid_tread_half_quantizer,
 )
 
+pytestmark = pytest.mark.usefixtures("reset_default_graph", "set_test_environment")
 
-def test_quantized_layers(reset_default_graph):
-    model_classes = [
-        LmnetV0Quantize,
-        LmnetV1Quantize,
-        DarknetQuantize,
-        LMFYoloQuantize,
-        YoloV2Quantize,
-    ]
 
-    quantizer_name = "QTZ_binary_mean_scaling"
+def quantizer_name_func():
+    return "QTZ_binary_mean_scaling"
+
+
+def network_kwargs_func(first_quantize=True, last_quantize=True):
     network_kwargs = {
         "classes": ['accordion', 'airplanes', 'anchor'],
-        "is_debug": True,
+        "is_debug": False,
         "activation_quantizer": linear_mid_tread_half_quantizer,
         "batch_size": 10,
         "data_format": 'NHWC',
         "image_size": [32, 32],
         "optimizer_class": tf.train.GradientDescentOptimizer,
-        "quantize_first_convolution": True,
-        "quantize_last_convolution": True,
+        "quantize_first_convolution": first_quantize,
+        "quantize_last_convolution": last_quantize,
         "weight_quantizer": binary_mean_scaling_quantizer,
         "activation_quantizer_kwargs": {
             'bit': 2,
             'max_value': 2
         }
     }
+    return network_kwargs
 
-    for model in model_classes:
-        network_kwargs["quantize_first_convolution"] = True
-        network_kwargs["quantize_last_convolution"] = True
 
-        with tf.variable_scope("", reuse=tf.AUTO_REUSE):
-            network1 = model(**network_kwargs)
+def test_lmnetv0quantize_quantize_both():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func()
 
-            network1.base(tf.ones([10, 32, 32, 3]), True)
-            graph1 = tf.get_default_graph()
-            op_name_list = [op.name for op in graph1.get_operations() if "kernel" in op.name]
-            scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
-            assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+    network = LmnetV0Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
 
-        tf.reset_default_graph()
-        network_kwargs["quantize_first_convolution"] = False
+    op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
 
-        with tf.variable_scope("", reuse=tf.AUTO_REUSE):
-            network2 = model(**network_kwargs)
 
-            network2.base(tf.ones([10, 32, 32, 3]), True)
-            graph2 = tf.get_default_graph()
-            op_name_list = [op.name for op in graph2.get_operations() if "kernel" in op.name]
-            assert not any(network2.first_layer_name in op and quantizer_name in op for op in op_name_list)
+def test_lmnetv0quantize_quantize_first():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(last_quantize=False)
 
-            op_name_list = [op_name for op_name in op_name_list if network2.first_layer_name not in op_name]
-            scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
-            assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+    network = LmnetV0Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
 
-        tf.reset_default_graph()
-        network_kwargs["quantize_first_convolution"] = True
-        network_kwargs["quantize_last_convolution"] = False
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.last_layer_name in op and quantizer_name in op for op in op_name_list)
 
-        with tf.variable_scope("", reuse=tf.AUTO_REUSE):
-            network3 = model(**network_kwargs)
+    op_name_list = [op_name for op_name in op_name_list if network.last_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
 
-            network3.base(tf.ones([10, 32, 32, 3]), True)
-            graph3 = tf.get_default_graph()
-            op_name_list = [op.name for op in graph3.get_operations() if "kernel" in op.name]
-            assert not any(network3.last_layer_name in op and quantizer_name in op for op in op_name_list)
 
-            op_name_list = [op_name for op_name in op_name_list if network3.last_layer_name not in op_name]
-            scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
-            assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+def test_lmnetv0quantize_quantize_last():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(first_quantize=False)
 
-        tf.reset_default_graph()
+    network = LmnetV0Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.first_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.first_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmnetv1quantize_quantize_both():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func()
+
+    network = LmnetV1Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmnetv1quantize_quantize_first():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(last_quantize=False)
+
+    network = LmnetV1Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.last_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.last_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmnetv1quantize_quantize_last():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(first_quantize=False)
+
+    network = LmnetV1Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.first_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.first_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_darknetquantize_quantize_both():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func()
+
+    network = DarknetQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_darknetquantize_quantize_first():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(last_quantize=False)
+
+    network = DarknetQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.last_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.last_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_darknetquantize_quantize_last():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(first_quantize=False)
+
+    network = DarknetQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.first_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.first_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmfyoloquantize_quantize_both():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func()
+
+    network = LMFYoloQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmfyoloquantize_quantize_first():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(last_quantize=False)
+
+    network = LMFYoloQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.last_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.last_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_lmfyoloquantize_quantize_last():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(first_quantize=False)
+
+    network = LMFYoloQuantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.first_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.first_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_yolov2quantize_quantize_both():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func()
+
+    network = YoloV2Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    op_name_list = [op.name for op in graph.get_operations() if "kernel" in op.name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_yolov2quantize_quantize_first():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(last_quantize=False)
+
+    network = YoloV2Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.last_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.last_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
+
+
+def test_yolov2quantize_quantize_last():
+    quantizer_name = quantizer_name_func()
+    network_kwargs = network_kwargs_func(first_quantize=False)
+
+    network = YoloV2Quantize(**network_kwargs)
+    network.base(tf.ones([10, 32, 32, 3]), True)
+    graph = tf.get_default_graph()
+
+    ops = graph.get_operations()
+    op_name_list = [op.name for op in ops if "kernel" in op.name]
+    assert not any(network.first_layer_name in op and quantizer_name in op for op in op_name_list)
+
+    op_name_list = [op_name for op_name in op_name_list if network.first_layer_name not in op_name]
+    scope_name_list = list(set([op.split("/")[0] for op in op_name_list]))
+    assert all(any(scope in op and quantizer_name in op for op in op_name_list) for scope in scope_name_list)
 
 
 if __name__ == '__main__':
-    test_quantized_layers()
+    test_lmnetv0quantize_quantize_both()
+    test_lmnetv0quantize_quantize_first()
+    test_lmnetv0quantize_quantize_last()
+    test_lmnetv1quantize_quantize_both()
+    test_lmnetv1quantize_quantize_first()
+    test_lmnetv1quantize_quantize_last()
+    test_darknetquantize_quantize_both()
+    test_darknetquantize_quantize_first()
+    test_darknetquantize_quantize_last()
+    test_lmfyoloquantize_quantize_both()
+    test_lmfyoloquantize_quantize_first()
+    test_lmfyoloquantize_quantize_last()
+    test_yolov2quantize_quantize_both()
+    test_yolov2quantize_quantize_first()
+    test_yolov2quantize_quantize_last()
