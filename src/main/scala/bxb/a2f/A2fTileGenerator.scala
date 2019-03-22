6a21063240ebe1c10ceb0f1c5e5eb5bbe6832043
Nikolay Nez <nikolay.nez@gmail.com>
2019-03-22 19:03:35 +0900

    add A2f: A2fTileGenerator

---
+++ src/main/scala/bxb/a2f/A2fTileGenerator.scala
@@ -0,0 +1,113 @@
+package bxb.a2f
+
+import chisel3._
+import chisel3.util._
+
+import bxb.util.{Util}
+
+class A2fTileGenerator(tileCountWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val start = Input(Bool())
+
+    // Tile generation parameters
+    val outputHCount = Input(UInt(6.W))
+    // - should be equal to roundUp(outputWidth / tileWidth)
+    val outputWCount = Input(UInt(6.W))
+
+    // tileHeight
+    val regularTileH = Input(UInt(tileCountWidth.W))
+    // - outputHeight - (hCount - 1)  * tileHeight
+    val lastTileH = Input(UInt(tileCountWidth.W))
+
+    // tileWidth
+    val regularTileW = Input(UInt(tileCountWidth.W))
+    // - outputWidth - (wCount - 1)  * tileWidth
+    val lastTileW = Input(UInt(tileCountWidth.W))
+
+    // Tile output interface with handshaking
+    val tileHeight = Output(UInt(tileCountWidth.W))
+    val tileWidth = Output(UInt(tileCountWidth.W))
+    val tileValid = Output(Bool())
+    val tileAccepted = Input(Bool())
+  })
+
+  // FIXME: it is too much for the task done
+  object State {
+    val idle :: resetCounters :: updateCounters :: setupTile :: valid :: Nil = Enum(5)
+  }
+
+  val state = RegInit(State.idle)
+  val idle = (state === State.idle)
+  val resetCounters = (state === State.resetCounters)
+  val updateCounters = (state === State.updateCounters)
+  val setupTile = (state === State.setupTile)
+  val valid = (state === State.valid)
+
+  val outputWCountLeft = Reg(UInt(tileCountWidth.W))
+  val outputWCountOne = (outputWCountLeft === 1.U)
+  val outputWCountLast = outputWCountOne
+  when(resetCounters) {
+    outputWCountLeft := io.outputWCount
+  }.elsewhen(updateCounters) {
+    when(outputWCountLast) {
+      outputWCountLeft := io.outputWCount
+    }.otherwise {
+      outputWCountLeft := outputWCountLeft - 1.U
+    }
+  }
+  val horizontalLast = outputWCountOne
+
+  val outputHCountLeft = Reg(UInt(tileCountWidth.W))
+  val outputHCountOne = (outputHCountLeft === 1.U)
+  val outputHCountLast = outputHCountOne & outputWCountLast
+  when(resetCounters) {
+    outputHCountLeft := io.outputHCount
+  }.elsewhen(updateCounters & outputWCountLast) {
+    when(outputHCountLast) {
+      outputHCountLeft := io.outputHCount
+    }.otherwise {
+      outputHCountLeft := outputHCountLeft - 1.U
+    }
+  }
+  val verticalLast = outputHCountOne
+
+  val tileWidth = Reg(UInt(tileCountWidth.W))
+  when(setupTile) {
+    when(~horizontalLast) {
+      tileWidth := io.regularTileW
+    }.otherwise {
+      tileWidth := io.lastTileW
+    }
+  }
+
+  val tileHeight = Reg(UInt(tileCountWidth.W))
+  when(setupTile) {
+    when(~verticalLast) {
+      tileHeight := io.regularTileH
+    }.otherwise {
+      tileHeight := io.lastTileH
+    }
+  }
+
+  when(idle & io.start) {
+    state := State.resetCounters
+  }.elsewhen(resetCounters) {
+    state := State.setupTile
+  }.elsewhen(updateCounters) {
+    state := State.setupTile
+  }.elsewhen(setupTile) {
+    state := State.valid
+  }.elsewhen(valid & io.tileAccepted) {
+    state := State.updateCounters
+  }
+
+  io.tileHeight := tileHeight
+  io.tileWidth := tileWidth
+  io.tileValid := valid
+}
+
+object A2fTileGenerator {
+  def main(args: Array[String]): Unit = {
+    println(Util.getVerilog(new A2fTileGenerator(12)))
+  }
+}
