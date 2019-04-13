ea571708bb50301e4faba37ea5e95c306ac689e1
Nikolay Nez <nikolay.nez@gmail.com>
2019-04-13 18:47:24 +0900

    add Bxb: integrate a2f pipeline

--- src/main/scala/bxb/Bxb.scala
+++ src/main/scala/bxb/Bxb.scala
@@ -6,10 +6,12 @@ import chisel3.util._
 import bxb.a2f.{A2f}
 import bxb.adma.{ADma}
 import bxb.array.{MacArray}
+import bxb.f2a.{F2a}
 import bxb.fdma.{FDma}
+import bxb.rdma.{RDma}
 import bxb.w2m.{W2m}
-import bxb.wqdma.{WDma}
-import bxb.sync.{SemaphorePair}
+import bxb.wqdma.{WDma, QDma}
+import bxb.sync.{SemaphorePair, ConsumerSyncMux}
 import bxb.memory.{MemArray, PackedBlockRam, TwoBlockMemArray}
 
 import bxb.util.{Util}
@@ -67,8 +69,10 @@ object BxbCsrField {
   val a2fLastTileH = 47
   val a2fRegularTileW = 48
   val a2fLastTileW = 49
+  val qdmaStartAddress = 50
+  val bnqEnable = 51
 
-  val parameterCount = 50
+  val parameterCount = 52
 
   // status registers
   val statusRegister = parameterCount + 0
@@ -121,6 +125,12 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
     val wdmaOutputWCount = Output(UInt(wCountWidth.W))
     val wdmaKernelBlockCount = Output(UInt(blockCountWidth.W))
 
+    // WDMA parameters
+    val qdmaStartAddress = Output(UInt(avalonAddrWidth.W))
+    val qdmaOutputHCount = Output(UInt(hCountWidth.W))
+    val qdmaOutputWCount = Output(UInt(wCountWidth.W))
+    val qdmaKernelBlockCount = Output(UInt(blockCountWidth.W))
+
     // FDMA parameters
     val fdmaOutputAddress = Output(UInt(avalonAddrWidth.W))
     val fdmaOutputHCount = Output(UInt(6.W))
@@ -134,6 +144,19 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
     val fdmaLastRowToRowDistance = Output(UInt(tileCountWidth.W))
     val fdmaOutputSpace = Output(UInt(avalonAddrWidth.W))
     val fdmaRowDistance = Output(UInt(avalonAddrWidth.W))
+
+    val rdmaOutputAddress = Output(UInt(avalonAddrWidth.W))
+    val rdmaOutputHCount = Output(UInt(6.W))
+    val rdmaOutputWCount = Output(UInt(6.W))
+    val rdmaOutputCCount = Output(UInt(6.W))
+    val rdmaRegularTileH = Output(UInt(tileCountWidth.W))
+    val rdmaLastTileH = Output(UInt(tileCountWidth.W))
+    val rdmaRegularTileW = Output(UInt(tileCountWidth.W))
+    val rdmaLastTileW = Output(UInt(tileCountWidth.W))
+    val rdmaRegularRowToRowDistance = Output(UInt(tileCountWidth.W))
+    val rdmaLastRowToRowDistance = Output(UInt(tileCountWidth.W))
+    val rdmaOutputSpace = Output(UInt(avalonAddrWidth.W))
+    val rdmaRowDistance = Output(UInt(avalonAddrWidth.W))
 
     // A2F parameters
     val a2fInputCCount = Output(UInt(6.W))
@@ -149,11 +172,25 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
     val a2fRegularTileW = Output(UInt(tileCountWidth.W))
     val a2fLastTileW = Output(UInt(tileCountWidth.W))
 
+    // F2A parameters
+    val f2rOutputHCount = Output(UInt(6.W))
+    val f2rOutputWCount = Output(UInt(6.W))
+    val f2rOutputCCount = Output(UInt(6.W))
+    val f2rRegularTileH = Output(UInt(tileCountWidth.W))
+    val f2rLastTileH = Output(UInt(tileCountWidth.W))
+    val f2rRegularTileW = Output(UInt(tileCountWidth.W))
+    val f2rLastTileW = Output(UInt(tileCountWidth.W))
+
+    val bnqEnable = Output(Bool())
+
     // Modules Status
     val a2fStatusReady = Input(Bool())
     val admaStatusReady = Input(Bool())
     val wdmaStatusReady = Input(Bool())
     val fdmaStatusReady = Input(Bool())
+    val qdmaStatusReady = Input(Bool())
+    val f2rStatusReady = Input(Bool())
+    val rdmaStatusReady = Input(Bool())
   })
 
   val field = RegInit(VecInit(Seq.fill(BxbCsrField.parameterCount){0.U(32.W)}))
@@ -162,14 +199,24 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
   }
 
   val readData = Reg(UInt(32.W))
+  val status = Cat(0.U(25.W),
+    /* 6 */ io.rdmaStatusReady,
+    /* 5 */ io.f2rStatusReady,
+    /* 4 */ io.qdmaStatusReady,
+    /* 3 */ io.a2fStatusReady,
+    /* 2 */ io.admaStatusReady,
+    /* 1 */ io.wdmaStatusReady,
+    /* 0 */ io.fdmaStatusReady
+  )
   when(io.avalonSlaveRead & (io.avalonSlaveAddress =/= BxbCsrField.statusRegister.U)) {
     readData := field(io.avalonSlaveAddress)
   }.otherwise {
-    readData := Cat(0.U(28.W), io.a2fStatusReady, io.admaStatusReady, io.wdmaStatusReady, io.fdmaStatusReady)
+    readData := status
   }
   io.avalonSlaveReadData := readData
 
   io.start := RegNext(io.avalonSlaveWrite & (io.avalonSlaveAddress === BxbCsrField.start.U))
+  // ADMA
   io.admaInputAddress := field(BxbCsrField.admaInputAddress.U)
   io.admaInputHCount := field(BxbCsrField.admaInputHCount.U)
   io.admaInputWCount := field(BxbCsrField.admaInputWCount.U)
@@ -193,10 +240,17 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
   io.admaTopBottomMiddlePad := field(BxbCsrField.admaTopBottomMiddlePad.U)
   io.admaTopBottomRightPad := field(BxbCsrField.admaTopBottomRightPad.U)
   io.admaSidePad := field(BxbCsrField.admaSidePad.U)
+  // WDMA
   io.wdmaStartAddress := field(BxbCsrField.wdmaStartAddress.U)
   io.wdmaOutputHCount := field(BxbCsrField.wdmaOutputHCount.U)
   io.wdmaOutputWCount := field(BxbCsrField.wdmaOutputWCount.U)
   io.wdmaKernelBlockCount := field(BxbCsrField.wdmaKernelBlockCount.U)
+  // QDMA
+  io.qdmaStartAddress := field(BxbCsrField.qdmaStartAddress.U)
+  io.qdmaOutputHCount := field(BxbCsrField.wdmaOutputHCount.U)
+  io.qdmaOutputWCount := field(BxbCsrField.wdmaOutputWCount.U)
+  io.qdmaKernelBlockCount := field(BxbCsrField.fdmaOutputCCount.U)
+  // FDMA
   io.fdmaOutputAddress := field(BxbCsrField.fdmaOutputAddress.U)
   io.fdmaOutputHCount := field(BxbCsrField.fdmaOutputHCount.U)
   io.fdmaOutputWCount := field(BxbCsrField.fdmaOutputWCount.U)
@@ -209,6 +263,20 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
   io.fdmaLastRowToRowDistance := field(BxbCsrField.fdmaLastRowToRowDistance.U)
   io.fdmaOutputSpace := field(BxbCsrField.fdmaOutputSpace.U)
   io.fdmaRowDistance := field(BxbCsrField.fdmaRowDistance.U)
+  // RDMA
+  io.rdmaOutputAddress := field(BxbCsrField.fdmaOutputAddress.U)
+  io.rdmaOutputHCount := field(BxbCsrField.fdmaOutputHCount.U)
+  io.rdmaOutputWCount := field(BxbCsrField.fdmaOutputWCount.U)
+  io.rdmaOutputCCount := field(BxbCsrField.fdmaOutputCCount.U)
+  io.rdmaRegularTileH := field(BxbCsrField.fdmaRegularTileH.U)
+  io.rdmaLastTileH := field(BxbCsrField.fdmaLastTileH.U)
+  io.rdmaRegularTileW := field(BxbCsrField.fdmaRegularTileW.U)
+  io.rdmaLastTileW := field(BxbCsrField.fdmaLastTileW.U)
+  io.rdmaRegularRowToRowDistance := field(BxbCsrField.fdmaRegularRowToRowDistance.U)
+  io.rdmaLastRowToRowDistance := field(BxbCsrField.fdmaLastRowToRowDistance.U)
+  io.rdmaOutputSpace := field(BxbCsrField.fdmaOutputSpace.U)
+  io.rdmaRowDistance := field(BxbCsrField.fdmaRowDistance.U)
+  // A2F
   io.a2fInputCCount := field(BxbCsrField.a2fInputCCount.U)
   io.a2fKernelVCount := field(BxbCsrField.a2fKernelVCount.U)
   io.a2fKernelHCount := field(BxbCsrField.a2fKernelHCount.U)
@@ -221,9 +289,19 @@ class BxbCsr(avalonAddrWidth: Int, tileCountWidth: Int) extends Module {
   io.a2fLastTileH := field(BxbCsrField.a2fLastTileH.U)
   io.a2fRegularTileW := field(BxbCsrField.a2fRegularTileW.U)
   io.a2fLastTileW := field(BxbCsrField.a2fLastTileW.U)
+  // F2R
+  io.f2rOutputHCount := field(BxbCsrField.a2fOutputHCount.U)
+  io.f2rOutputWCount := field(BxbCsrField.a2fOutputWCount.U)
+  io.f2rOutputCCount := field(BxbCsrField.fdmaOutputCCount.U)
+  io.f2rRegularTileH := field(BxbCsrField.a2fRegularTileH.U)
+  io.f2rLastTileH := field(BxbCsrField.a2fLastTileH.U)
+  io.f2rRegularTileW := field(BxbCsrField.a2fRegularTileW.U)
+  io.f2rLastTileW := field(BxbCsrField.a2fLastTileW.U)
+  // BNQ Enable
+  io.bnqEnable := field(BxbCsrField.bnqEnable)(0)
 }
 
-class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
+class Bxb(dataMemSize: Int, wmemSize: Int, qmemSize: Int) extends Module {
   require(isPow2(dataMemSize))
   require(isPow2(wmemSize))
 
@@ -235,11 +313,15 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
   val wEntriesCount = wmemSize / b
   val wSemaWidth = Chisel.log2Floor(wEntriesCount) + 1
 
+  val qEntriesCount = qmemSize
+  val qSemaWidth = Chisel.log2Floor(qEntriesCount) + 1
+
   val avalonAddrWidth = 32
   val maxBurst = 32
 
   val dataAddrWidth = Chisel.log2Ceil(dataMemSize)
   val wAddrWidth = Chisel.log2Ceil(wmemSize)
+  val qAddrWidth = Chisel.log2Ceil(qmemSize)
   val tileCountWidth = dataAddrWidth
 
   // FIXME: rid of copypaste
@@ -272,12 +354,27 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
     val wdmaAvalonReadDataValid = Input(Bool())
     val wdmaAvalonReadData = Input(UInt((b * wWidth).W))
 
+    // QDMA Avalon Interface
+    val qdmaAvalonAddress = Output(UInt(avalonAddrWidth.W))
+    val qdmaAvalonRead = Output(Bool())
+    val qdmaAvalonBurstCount = Output(UInt(10.W))
+    val qdmaAvalonWaitRequest = Input(Bool())
+    val qdmaAvalonReadDataValid = Input(Bool())
+    val qdmaAvalonReadData = Input(UInt(64.W))
+
     // FDMA Avalon Interface
     val fdmaAvalonAddress = Output(UInt(avalonAddrWidth.W))
     val fdmaAvalonBurstCount = Output(UInt(10.W))
     val fdmaAvalonWaitRequest = Input(Bool())
     val fdmaAvalonWrite = Output(Bool())
     val fdmaAvalonWriteData = Output(UInt(128.W))
+
+    // RDMA Avalon Interface
+    val rdmaAvalonAddress = Output(UInt(avalonAddrWidth.W))
+    val rdmaAvalonBurstCount = Output(UInt(10.W))
+    val rdmaAvalonWaitRequest = Input(Bool())
+    val rdmaAvalonWrite = Output(Bool())
+    val rdmaAvalonWriteData = Output(UInt((b * aWidth).W))
   })
 
   val csr = Module(new BxbCsr(avalonAddrWidth, tileCountWidth))
@@ -296,8 +393,21 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
   val msema = Module(new SemaphorePair(2, 0, 2))
   val macArray = Module(new MacArray(b, fWidth, aWidth))
 
+  val fsemaConsumerMux = Module(new ConsumerSyncMux)
+  fsemaConsumerMux.io.select := csr.io.bnqEnable
   val fsema = Module(new SemaphorePair(2, 0, 2))
+  fsema.io.consumer <> fsemaConsumerMux.io.out
+
   val fmem = Module(new TwoBlockMemArray(b, dataMemSize, fWidth))
+  val fdmaRead = Wire(fmem.io.readB.cloneType)
+  val f2rRead = Wire(fmem.io.readB.cloneType)
+  fmem.io.readB := Mux(csr.io.bnqEnable, f2rRead, fdmaRead) 
+
+  val qsema = Module(new SemaphorePair(qSemaWidth, 0, qEntriesCount))
+  val qmem = Module(new PackedBlockRam(b, qmemSize, 40))
+
+  val rsema = Module(new SemaphorePair(2, 0, 2))
+  val rmem = Module(new MemArray(b, dataMemSize, aWidth))
 
   val adma = Module(new ADma(b, dataAddrWidth, avalonAddrWidth, maxBurst))
   adma.io.start := csr.io.start
@@ -368,11 +478,11 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
   csr.io.wdmaStatusReady := wdma.io.statusReady
 
   val fdma = Module(new FDma(b, dataAddrWidth, avalonAddrWidth, 128, maxBurst))
-  fdma.io.start := csr.io.start
+  fdma.io.start := ~csr.io.bnqEnable & csr.io.start
   // FIXME: refactor sync interface
-  fdma.io.fRawZero := fsema.io.consumer.rawZero
-  fsema.io.consumer.rawDec := fdma.io.fRawDec
-  fsema.io.consumer.warInc := fdma.io.fWarInc
+  fdma.io.fRawZero := fsemaConsumerMux.io.b.rawZero
+  fsemaConsumerMux.io.b.rawDec := fdma.io.fRawDec
+  fsemaConsumerMux.io.b.warInc := fdma.io.fWarInc
 
   // FIXME: refactor avalon interface
   io.fdmaAvalonAddress := fdma.io.avalonMasterAddress
@@ -396,9 +506,66 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
   fdma.io.rowDistance := csr.io.fdmaRowDistance
 
   // FIXME: refactor memory interface
-  fmem.io.readB := fdma.io.fmemRead
+  fdmaRead := fdma.io.fmemRead
   fdma.io.fmemQ := fmem.io.qB
   csr.io.fdmaStatusReady := fdma.io.statusReady
+
+  val rdma = Module(new RDma(b, dataAddrWidth, avalonAddrWidth, maxBurst))
+  rdma.io.start := csr.io.bnqEnable & csr.io.start
+  // FIXME: refactor sync interface
+  rdma.io.rRawZero := rsema.io.consumer.rawZero
+  rsema.io.consumer.rawDec := rdma.io.rRawDec
+  rsema.io.consumer.warInc := rdma.io.rWarInc
+
+  // FIXME: refactor avalon interface
+  io.rdmaAvalonAddress := rdma.io.avalonMasterAddress
+  io.rdmaAvalonBurstCount := rdma.io.avalonMasterBurstCount
+  rdma.io.avalonMasterWaitRequest := io.rdmaAvalonWaitRequest
+  io.rdmaAvalonWrite := rdma.io.avalonMasterWrite
+  io.rdmaAvalonWriteData := rdma.io.avalonMasterWriteData
+
+  // FIXME: refactor parameters
+  rdma.io.outputAddress := csr.io.rdmaOutputAddress
+  rdma.io.outputHCount := csr.io.rdmaOutputHCount
+  rdma.io.outputWCount := csr.io.rdmaOutputWCount
+  rdma.io.outputCCount := csr.io.rdmaOutputCCount
+  rdma.io.regularTileH := csr.io.rdmaRegularTileH
+  rdma.io.lastTileH := csr.io.rdmaLastTileH
+  rdma.io.regularTileW := csr.io.rdmaRegularTileW
+  rdma.io.lastTileW := csr.io.rdmaLastTileW
+  rdma.io.regularRowToRowDistance := csr.io.rdmaRegularRowToRowDistance
+  rdma.io.lastRowToRowDistance := csr.io.rdmaLastRowToRowDistance
+  rdma.io.outputSpace := csr.io.rdmaOutputSpace
+  rdma.io.rowDistance := csr.io.rdmaRowDistance
+
+  // FIXME: refactor memory interface
+  rmem.io.read := rdma.io.rmemRead
+  rdma.io.rmemQ := rmem.io.q
+  csr.io.rdmaStatusReady := rdma.io.statusReady
+
+  val qdma = Module(new QDma(b, avalonAddrWidth, 64, qAddrWidth))
+  qdma.io.start := csr.io.bnqEnable & csr.io.start
+  // FIXME: refactor sync interface
+  qdma.io.qWarZero := qsema.io.producer.warZero
+  qsema.io.producer.warDec := qdma.io.qWarDec
+  qsema.io.producer.rawInc := qdma.io.qRawInc
+
+  // FIXME: refactor avalon interface
+  io.qdmaAvalonAddress := qdma.io.avalonMasterAddress
+  io.qdmaAvalonRead := qdma.io.avalonMasterRead
+  io.qdmaAvalonBurstCount := qdma.io.avalonMasterBurstCount
+  qdma.io.avalonMasterWaitRequest := io.qdmaAvalonWaitRequest
+  qdma.io.avalonMasterReadDataValid := io.qdmaAvalonReadDataValid
+  qdma.io.avalonMasterReadData := io.qdmaAvalonReadData
+
+  // FIXME: refactor parameters
+  qdma.io.startAddress := csr.io.qdmaStartAddress
+  qdma.io.outputHCount := csr.io.qdmaOutputHCount
+  qdma.io.outputWCount := csr.io.qdmaOutputWCount
+  qdma.io.kernelBlockCount := csr.io.qdmaKernelBlockCount
+
+  qmem.io.write := qdma.io.qmemWrite
+  csr.io.qdmaStatusReady := qdma.io.statusReady
 
   val w2m = Module(new W2m(b, wmemSize))
   wsema.io.consumer <> w2m.io.wSync
@@ -445,10 +612,35 @@ class Bxb(dataMemSize: Int, wmemSize: Int) extends Module {
   a2f.io.regularTileW := csr.io.a2fRegularTileW
   a2f.io.lastTileW := csr.io.a2fLastTileW
   csr.io.a2fStatusReady := a2f.io.statusReady
+
+  val f2r = Module(new F2a(b, dataMemSize, qmemSize, aWidth, fWidth))
+  f2r.io.start := csr.io.bnqEnable & csr.io.start
+
+  f2r.io.outputHCount := csr.io.f2rOutputHCount
+  f2r.io.outputWCount := csr.io.f2rOutputWCount
+  f2r.io.outputCCount := csr.io.f2rOutputCCount
+  f2r.io.regularTileH := csr.io.f2rRegularTileH
+  f2r.io.lastTileH := csr.io.f2rLastTileH
+  f2r.io.regularTileW := csr.io.f2rRegularTileW
+  f2r.io.lastTileW := csr.io.f2rLastTileW
+
+  rmem.io.write := f2r.io.amemWrite
+
+  f2rRead := f2r.io.fmemRead
+  f2r.io.fmemQ := fmem.io.qB
+
+  qmem.io.read := f2r.io.qmemRead
+  f2r.io.qmemQ := qmem.io.q
+
+  rsema.io.producer <> f2r.io.aSync
+  qsema.io.consumer <> f2r.io.qSync
+  fsemaConsumerMux.io.a <> f2r.io.fSync
+
+  csr.io.f2rStatusReady := f2r.io.statusReady
 }
 
 object Bxb {
   def main(args: Array[String]): Unit = {
-    println(Util.getVerilog(new Bxb(4 * 1024, 1024)))
+    println(Util.getVerilog(new Bxb(4 * 1024, 1024, 32)))
   }
 }
