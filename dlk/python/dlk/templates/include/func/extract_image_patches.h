c0996986aac6b57faf16a009f4af2f004b025a85
primenumber <--global>
2019-05-10 16:57:48 +0900

    Fix extract_image_patches

--- dlk/python/dlk/templates/include/func/extract_image_patches.h
+++ dlk/python/dlk/templates/include/func/extract_image_patches.h
@@ -22,48 +22,56 @@ limitations under the License.
 #include "pack_input_to_qwords.h"
 #include <limits.h>
 
-template<class T>
-void func_ExtractImagePatches(
-    const TensorView<T, MemoryLayout::ChHWBCl>& input,
-    const TensorView<T, MemoryLayout::ChHWBCl>& output,
+inline void func_ExtractImagePatches(
+    const TensorView<QUANTIZED_PACKED, MemoryLayout::ChHWBCl>& input,
+    const TensorView<QUANTIZED_PACKED, MemoryLayout::ChHWBCl>& output,
     T_UINT kernel_size, T_UINT stride)
 {
   Measurement::Start("ExtractImagePatches");
   const auto in_shape = input.get_shape();
   const T_UINT input_width = in_shape[2];
-  const T_UINT input_depth = in_shape[3];
+  const T_UINT input_depth = in_shape[0];
+  const T_UINT bits_per_input = in_shape[3];
   const auto out_shape = output.get_shape();
   const T_UINT out_height = out_shape[1];
   const T_UINT out_width = out_shape[2];
-  const T_UINT out_depth = out_shape[3];
+  const T_UINT out_depth = out_shape[0];
 
-  const int bits_per_input = 2;
-  const int bits_per_word = T::BitCount;
-  int full_words_in_depth = input_depth / bits_per_word;
-  int remainder_bits_in_depth = input_depth % bits_per_word;
-
-  T_UINT packed_input_depth = full_words_in_depth * bits_per_input + (remainder_bits_in_depth ? bits_per_input : 0);
-  T_UINT packed_output_size = packed_input_depth * kernel_size * kernel_size * out_width * out_height;
-
-  auto* out = output.data();
   T_UINT output_index = 0;
 
-  for(T_UINT ih = 0; ih < input_depth; ++ih)
-    for(T_UINT ki = 0; ki < kernel_size; ki++)
-      for(T_UINT kj = 0; kj < kernel_size; kj++)
-        for(T_UINT wi = 0; wi < out_height; wi++)
-          for(T_UINT wj = 0; wj < out_width; wj++)
+  if (out_depth < kernel_size * kernel_size) {
+    int bit_shift = out_depth * QUANTIZED_PACKED::BitCount / (kernel_size * kernel_size);
+    const QUANTIZED_PACKED::base_t mask((QUANTIZED_PACKED::base_t(1) << bit_shift) - 1);
+    for(T_UINT wi = 0; wi < out_height; wi++)
+      for(T_UINT wj = 0; wj < out_width; wj++)
+        for(T_UINT ki = 0; ki < kernel_size; ki++)
+          for(T_UINT kj = 0; kj < kernel_size; kj++)
           {
+            T_INT row = (wi * stride) + ki;
+            T_INT col = (wj * stride) + kj;
+            T_UINT ch = (ki * kernel_size + kj) * bit_shift;
+            T_UINT ch_high = ch / QUANTIZED_PACKED::BitCount;
+            T_UINT ch_low = ch % QUANTIZED_PACKED::BitCount;
             for(T_UINT digit = 0; digit < bits_per_input; ++digit) {
-              for(T_UINT kz = 0; kz < bits_per_word; kz++)
-              {
-                T_INT row = (wi * stride) + ki;
-                T_INT col = (wj * stride) + kj;
-
-                out[output_index++] = input(ih, row, col, digit, kz);
-              }
+              output(ch_high, wi, wj, digit, 0) |= T((mask & input(0, row, col, digit, 0).Raw()) << ch_low);
             }
           }
+  } else {
+    for(T_UINT ih = 0; ih < input_depth; ++ih)
+      for(T_UINT wi = 0; wi < out_height; wi++)
+        for(T_UINT wj = 0; wj < out_width; wj++)
+          for(T_UINT ki = 0; ki < kernel_size; ki++)
+            for(T_UINT kj = 0; kj < kernel_size; kj++)
+            {
+              T_INT row = (wi * stride) + ki;
+              T_INT col = (wj * stride) + kj;
+              for(T_UINT digit = 0; digit < bits_per_input; ++digit) {
+                output(ih * kernel_size * kernel_size
+                  + ki * kernel_size + kj, wi, wj,
+                  digit, 0) = input(ih, row, col, digit, 0);
+              }
+            }
+  }
 
   Measurement::Stop();
 }
