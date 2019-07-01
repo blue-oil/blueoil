c3689d4e63b88d12c9a27291ee7a21cbe204bedf
prime number <prime@kmc.gr.jp>
2019-07-01 20:17:41 +0900

    Add toolchain for aarch64 (#337)

---
+++ dlk/python/dlk/templates/cmake/toolchain/linux_aarch64.cmake
@@ -0,0 +1,28 @@
+#
+# Please, if you want to use a specific toolchain and sysroot
+# define TOOLCHAIN_PATH and CMAKE_SYSROOT variables:
+#
+set(TOOLCHAIN_PATH "")
+set(CMAKE_SYSROOT "")
+
+
+#
+# Cross-compiling for ARM
+#
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+if(NOT TOOLCHAIN_PATH)
+    set(TOOLCHAIN_BIN_PATH "")
+else()
+    set(TOOLCHAIN_BIN_PATH ${TOOLCHAIN_PATH}/bin/)
+endif()
+
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_BIN_PATH}aarch64-linux-gnu-g++)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_PATH}aarch64-linux-gnu-g++)
+set(CMAKE_CXX_COMPILER_AR ${TOOLCHAIN_BIN_PATH}aarch64-linux-gnu-ar)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
