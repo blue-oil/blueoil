18c80d960fd32e9e80442678e0107038184ee2e3
primenumber <--global>
2019-07-03 16:18:28 +0900

    Optimize for aarch32

--- dlk/python/dlk/templates/src/func/impl/arm_neon/pack_16bit.cpp
+++ dlk/python/dlk/templates/src/func/impl/arm_neon/pack_16bit.cpp
@@ -27,6 +27,59 @@ void pack_16bit(const BIN_CONV_OUTPUT input[], QUANTIZED_PACKED output[], const 
   const auto bits = QUANTIZED_PACKED::BitCount;
   assert((length % bits) == 0);
   std::size_t i = 0, j = 0;
+#ifdef AARCH32
+  constexpr std::size_t SIMD_WIDTH = 64; // hardcoded, not configurable
+  constexpr uint8_t coeff_ary[16] = {
+    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
+    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
+  };
+  const auto coeff = vld1q_u8(coeff_ary);
+  for (; i < length; i += SIMD_WIDTH) {
+    // b0
+    const auto a0 = vld2q_u8(reinterpret_cast<const uint8_t*>(input + i + 0x00)).val[0];
+    const auto am0 = vmulq_u8(vshrq_n_u8(a0, 1), coeff);
+    const auto al0 = vmulq_u8(vandq_u8(a0, vdupq_n_u8(0x01)), coeff);
+    const auto bm0 = vreinterpretq_u8_u16(vpaddlq_u8(am0));
+    const auto bl0 = vreinterpretq_u8_u16(vpaddlq_u8(al0));
+    // b1
+    const auto a1 = vld2q_u8(reinterpret_cast<const uint8_t*>(input + i + 0x10)).val[0];
+    const auto am1 = vmulq_u8(vshrq_n_u8(a1, 1), coeff);
+    const auto al1 = vmulq_u8(vandq_u8(a1, vdupq_n_u8(0x01)), coeff);
+    const auto bm1 = vreinterpretq_u8_u16(vpaddlq_u8(am1));
+    const auto bl1 = vreinterpretq_u8_u16(vpaddlq_u8(al1));
+    // d01
+    const auto cm01 = vuzpq_u8(bm0, bm1).val[0];
+    const auto cl01 = vuzpq_u8(bl0, bl1).val[0];
+    const auto dm01 = vreinterpretq_u8_u16(vpaddlq_u8(cm01));
+    const auto dl01 = vreinterpretq_u8_u16(vpaddlq_u8(cl01));
+    // b2
+    const auto a2 = vld2q_u8(reinterpret_cast<const uint8_t*>(input + i + 0x20)).val[0];
+    const auto am2 = vmulq_u8(vshrq_n_u8(a2, 1), coeff);
+    const auto al2 = vmulq_u8(vandq_u8(a2, vdupq_n_u8(0x01)), coeff);
+    const auto bm2 = vreinterpretq_u8_u16(vpaddlq_u8(am2));
+    const auto bl2 = vreinterpretq_u8_u16(vpaddlq_u8(al2));
+    // b3
+    const auto a3 = vld2q_u8(reinterpret_cast<const uint8_t*>(input + i + 0x30)).val[0];
+    const auto am3 = vmulq_u8(vshrq_n_u8(a3, 1), coeff);
+    const auto al3 = vmulq_u8(vandq_u8(a3, vdupq_n_u8(0x01)), coeff);
+    const auto bm3 = vreinterpretq_u8_u16(vpaddlq_u8(am3));
+    const auto bl3 = vreinterpretq_u8_u16(vpaddlq_u8(al3));
+    // d23
+    const auto cm23 = vuzpq_u8(bm2, bm3).val[0];
+    const auto cl23 = vuzpq_u8(bl2, bl3).val[0];
+    const auto dm23 = vreinterpretq_u8_u16(vpaddlq_u8(cm23));
+    const auto dl23 = vreinterpretq_u8_u16(vpaddlq_u8(cl23));
+    // g
+    const auto em = vuzpq_u8(dm01, dm23).val[0];
+    const auto el = vuzpq_u8(dl01, dl23).val[0];
+    const auto fm = vreinterpret_u32_u8(vmovn_u16(vpaddlq_u8(em)));
+    const auto fl = vreinterpret_u32_u8(vmovn_u16(vpaddlq_u8(el)));
+    const auto g = vzip_u32(fl, fm);
+    vst1_u32(reinterpret_cast<uint32_t*>(output + j), g.val[0]);
+    vst1_u32(reinterpret_cast<uint32_t*>(output + j + 2), g.val[1]);
+    j += 4;
+  }
+#else
   constexpr std::size_t SIMD_WIDTH = 128; // hardcoded, not configurable
   constexpr uint8_t coeff_ary[16] = {
     0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
@@ -114,11 +167,12 @@ void pack_16bit(const BIN_CONV_OUTPUT input[], QUANTIZED_PACKED output[], const 
     const auto fl4567 = vreinterpretq_u8_u16(vpaddlq_u8(el4567));
     // g
     uint32x4x2_t g;
-    g.val[1] = vuzpq_u8(fm0123, fm4567).val[0];
-    g.val[0] = vuzpq_u8(fl0123, fl4567).val[0];
+    g.val[1] = vreinterpretq_u32_u8(vuzpq_u8(fm0123, fm4567).val[0]);
+    g.val[0] = vreinterpretq_u32_u8(vuzpq_u8(fl0123, fl4567).val[0]);
     vst2q_u32(output + j, g);
-    j += 8
+    j += 8;
   }
+#endif
   QUANTIZED_PACKED lsb(0), msb(0);
   for (; i < length; i += bits) {
     for (std::size_t i2 = 0; i2 < bits; ++i2) {
