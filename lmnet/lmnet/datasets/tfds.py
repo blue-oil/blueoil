b6deeeaa5626ac5bf12418b848f07dd6b501f872
Taketoshi Fujiwara <fujiwara@leapmind.io>
2019-07-16 20:59:16 +0900

    Added TFDSObjectDetection class

--- lmnet/lmnet/datasets/tfds.py
+++ lmnet/lmnet/datasets/tfds.py
@@ -13,22 +13,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-from lmnet.datasets.base import Base
+from lmnet.datasets.base import Base, ObjectDetectionBase
 
 
-def _label_to_one_hot(record, depth):
-    return {
-        'image': record['image'],
-        'label': tf.one_hot(record['label'], depth)
-    }
+def _format_classification_record(record, image_size, num_classes):
+    image = tf.image.resize(record["image"], image_size)
+    label = tf.one_hot(record["label"], num_classes)
+
+    return {"image": image, "label": label}
 
 
-class TFDSBase(Base):
+def _format_object_detection_record(record, image_size, num_max_boxes):
+    image = tf.image.resize(record["image"], image_size)
+
+    # Convert coordinates from relative to absolute
+    ymin = tf.slice(record["objects"]["bbox"], [0, 0], [-1, 1])
+    xmin = tf.slice(record["objects"]["bbox"], [0, 1], [-1, 1])
+    ymax = tf.slice(record["objects"]["bbox"], [0, 2], [-1, 1])
+    xmax = tf.slice(record["objects"]["bbox"], [0, 3], [-1, 1])
+
+    ymin = tf.cast(ymin * image_size[0], tf.int64)
+    xmin = tf.cast(xmin * image_size[1], tf.int64)
+    ymax = tf.cast(ymax * image_size[0], tf.int64)
+    xmax = tf.cast(xmax * image_size[1], tf.int64)
+
+    height = ymax - ymin
+    width  = xmax - xmin
+
+    # Combine boxes and labels
+    label = tf.expand_dims(record["objects"]["label"], axis=1)
+    gt_boxes = tf.concat([xmin, ymin, width, height, label], axis=1)
+
+    # Fill gt_boxes with dummy boxes
+    dummy_boxes = tf.stack([tf.constant([0, 0, 0, 0, -1], tf.int64)] * num_max_boxes, axis=0)
+    gt_boxes = tf.concat([gt_boxes, dummy_boxes], axis=0)
+    gt_boxes = tf.slice(gt_boxes, [0, 0], [num_max_boxes, 5])
+
+    return {"image": image, "label": gt_boxes}
+
+
+class TFDSMixin:
     """
-    Abstract dataset class for loading TensorFlow Datasets.
+    A Mixin to compose dataset classes for TFDS.
     Only images which has 3 channels (RGB) can be loaded for now.
     TODO(fujiwara): Convert grayscale images into RGB images.
     """
@@ -40,6 +70,8 @@ class TFDSBase(Base):
             tfds_name,
             tfds_data_dir,
             tfds_download=False,
+            tfds_image_size=None,
+            num_max_boxes=100,
             *args,
             **kwargs
     ):
@@ -57,23 +89,14 @@ class TFDSBase(Base):
         self._validate_feature_structure()
 
         self.tf_dataset = builder.as_dataset(split=self.available_splits[self.subset])
-        self.tf_dataset = self.tf_dataset.map(
-            lambda record: _label_to_one_hot(record, self.num_classes),
-            num_parallel_calls=tf.data.experimental.AUTOTUNE
-        )
+        self.tfds_image_size = tfds_image_size
+        self._num_max_boxes = num_max_boxes
+        self._format_dataset()
 
     @property
     def num_per_epoch(self):
         split = self.available_splits[self.subset]
         return self.info.splits[split].num_examples
-
-    @property
-    def classes(self):
-        return self.info.features["label"].names
-
-    @property
-    def num_classes(self):
-        return self.info.features["label"].num_classes
 
     @property
     def __getitem__(self, i):
@@ -114,12 +137,26 @@ class TFDSBase(Base):
         """
         raise NotImplementedError()
 
+    def _format_dataset(self):
+        """
+        Converting the format of loaded dataset.
+        """
+        raise NotImplementedError()
 
-class TFDSClassification(TFDSBase):
+
+class TFDSClassification(TFDSMixin, Base):
     """
     A dataset class for loading TensorFlow Datasets for classification.
     TensorFlow Datasets which have "label" and "image" features can be loaded by this class.
     """
+    @property
+    def classes(self):
+        return self.info.features["label"].names
+
+    @property
+    def num_classes(self):
+        return self.info.features["label"].num_classes
+
     def _validate_feature_structure(self):
         is_valid = \
             "label" in self.info.features and \
@@ -129,3 +166,52 @@ class TFDSClassification(TFDSBase):
 
         if not is_valid:
             raise ValueError("Datasets should have \"label\" and \"image\" features.")
+
+    def _format_dataset(self):
+        self.tf_dataset = self.tf_dataset.map(
+            lambda record: _format_classification_record(record, self.tfds_image_size, self.num_classes),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+
+
+class TFDSObjectDetection(TFDSMixin, ObjectDetectionBase):
+    """
+    A dataset class for loading TensorFlow Datasets for object detection.
+    TensorFlow Datasets which have "objects" and "image" features can be loaded by this class.
+    """
+    @classmethod
+    def count_max_boxes(cls):
+        raise NotImplementedError()
+
+    @property
+    def classes(self):
+        return self.info.features["objects"]["label"].names
+
+    @property
+    def num_classes(self):
+        return self.info.features["objects"]["label"].num_classes
+
+    @property
+    def num_max_boxes(self):
+        return self._num_max_boxes
+
+    def _validate_feature_structure(self):
+        is_valid = \
+            "image"   in self.info.features and \
+            "objects" in self.info.features and \
+            "label"   in self.info.features["objects"] and \
+            "bbox"    in self.info.features["objects"] and \
+            isinstance(self.info.features["image"],            tfds.features.Image) and \
+            isinstance(self.info.features["objects"],          tfds.features.SequenceDict) and \
+            isinstance(self.info.features["objects"]["label"], tfds.features.ClassLabel) and \
+            isinstance(self.info.features["objects"]["bbox"],  tfds.features.BBoxFeature)
+
+        if not is_valid:
+            raise ValueError("Datasets should have \"objects\" and \"image\" features and " \
+                             "\"objects\" should be a SequenceDict containing \"label\" and \"bbox\".")
+
+    def _format_dataset(self):
+        self.tf_dataset = self.tf_dataset.map(
+            lambda record: _format_object_detection_record(record, self.tfds_image_size, self.num_max_boxes),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
