62f28fbe69b3c581a150de1a8fdf2ecf7d0e3f58
primenumber <--global>
2019-07-31 15:39:36 +0900

    Optimize pack_input

--- dlk/python/dlk/templates/src/pack_input_to_qwords.cpp
+++ dlk/python/dlk/templates/src/pack_input_to_qwords.cpp
@@ -18,6 +18,12 @@ limitations under the License.
 #include "pack2bits.h"
 #include "operators.h" // FIXME(nikolay): for convolution_parameters definition, rid of it later
 #include "time_measurement.h"
+#ifdef USE_NEON
+#include <arm_neon.h>
+#endif
+#ifdef USE_AVX
+#include <x86intrin.h>
+#endif
 
 int pack_input(QUANTIZED_NOT_PACKED input[], size_t input_height, size_t input_width, size_t input_depth,
   size_t bits_per_input, QUANTIZED_PACKED output[]) {
@@ -30,11 +36,62 @@ int pack_input(QUANTIZED_NOT_PACKED input[], size_t input_height, size_t input_w
   int current_word = 0;
 
   auto len = input_height * input_width * input_depth;
-#ifdef AARCH32
+#ifdef USE_NEON
   if (input_depth % 32 == 0) {
-      pack2bits(input, output, (int)len);
-      Measurement::Stop();
-      return 0;
+    const uint8_t coeff_ary[16] = {
+      1, 2, 4, 8, 16, 32, 64, 128,
+      1, 2, 4, 8, 16, 32, 64, 128,
+    };
+    const auto coeff = vld1q_u8(coeff_ary);
+    const auto vone = vdupq_n_u8(1);
+    constexpr int b = 32;
+    constexpr int n_bits = 2;
+    const auto blocks = len / b;
+#pragma omp parallel for
+    for (int i = 0; i < blocks; ++i) {
+      const auto v0 = vld1q_u8(input + i * b +  0);
+      const auto v1 = vld1q_u8(input + i * b + 16);
+      const auto l0 = vandq_u8(v0, vone);
+      const auto l1 = vandq_u8(v1, vone);
+      const auto m0 = vshrq_n_u8(v0, 1);
+      const auto m1 = vshrq_n_u8(v1, 1);
+      const auto ml0 = vmulq_u8(l0, coeff);
+      const auto ml1 = vmulq_u8(l1, coeff);
+      const auto mm0 = vmulq_u8(m0, coeff);
+      const auto mm1 = vmulq_u8(m1, coeff);
+      const auto al0 = vpadd_u8(vget_low_u8(ml0), vget_high_u8(ml0));
+      const auto al1 = vpadd_u8(vget_low_u8(ml1), vget_high_u8(ml1));
+      const auto am0 = vpadd_u8(vget_low_u8(mm0), vget_high_u8(mm0));
+      const auto am1 = vpadd_u8(vget_low_u8(mm1), vget_high_u8(mm1));
+      const auto bl = vpadd_u8(al0, al1);
+      const auto bm = vpadd_u8(am0, am1);
+      const auto c = vpadd_u8(bl, bm);
+      vst1_u8(reinterpret_cast<uint8_t*>(output + i * n_bits), c);
+    }
+    Measurement::Stop();
+    return 0;
+  }
+#endif
+
+#ifdef USE_AVX
+  constexpr std::size_t SIMD_WIDTH = 32;
+  if ((len % SIMD_WIDTH) == 0) {
+    const auto blocks = len / SIMD_WIDTH;
+    const auto coeff = _mm256_setr_epi8(
+        1, 2, 4, 8, 16, 32, 64, 128,
+        1, 2, 4, 8, 16, 32, 64, 128,
+        1, 2, 4, 8, 16, 32, 64, 128,
+        1, 2, 4, 8, 16, 32, 64, 128
+    );
+    for (int i = 0; i < blocks; ++i) {
+      const auto a = _mm256_loadu_si256(reinterpret_cast<__m256i*>(input + i * SIMD_WIDTH));
+      const auto l = _mm256_movemask_epi8(_mm256_slli_epi16(a, 7));
+      const auto m = _mm256_movemask_epi8(_mm256_slli_epi16(a, 6));
+      output[i*2 + 0] = QUANTIZED_PACKED(l);
+      output[i*2 + 1] = QUANTIZED_PACKED(m);
+    }
+    Measurement::Stop();
+    return 0;
   }
 #endif
 
