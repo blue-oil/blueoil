ff0373fdd78a4c1563e29a84fafc341a3ccb7b4c
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-08-29 18:06:35 +0900

    Add more network structure

--- lmnet/lmnet/networks/optical_flow_estimation/flownet_s_v1.py
+++ lmnet/lmnet/networks/optical_flow_estimation/flownet_s_v1.py
@@ -39,6 +39,8 @@ class FlowNetSV1(BaseNetwork):
         self.weight_decay_rate = 0.0004
         self.use_batch_norm = True
         self.custom_getter = None
+        # TODO Where should I put the c files and where do we compile custom ops?
+        self.downsample_so = tf.load_op_library("downsample.so")
 
     # TODO: Import _conv_bn_act from blocks after replacing strides=2 using space to depth.
     def _conv_bn_act(
@@ -108,7 +110,8 @@ class FlowNetSV1(BaseNetwork):
                 strides=2,
                 padding='SAME',
                 use_bias=True,
-                biases_initializer=None
+                biases_initializer=None,
+                kernel_regularizer=tf.contrib.layers.l2_regularizer(self.weight_decay_rate)
             )
             output = self.activation(conved)
             return output
@@ -144,6 +147,37 @@ class FlowNetSV1(BaseNetwork):
                 padding='SAME',
                 use_bias=False
             )
+
+    def _downsample(
+            self,
+            name,
+            inputs,
+            size
+    ):
+        with tf.variable_scope(name):
+            return self.downsample_so.downsample(inputs, size)
+
+    def _average_endpoint_error(
+            self,
+            output,
+            labels
+    ):
+        """
+        Given labels and outputs of size (batch_size, height, width, 2), calculates average endpoint error:
+            sqrt{sum_across_the_2_channels[(X - Y)^2]}
+        """
+        batch_size = output.get_shape()[0]
+        with tf.name_scope(None, "average_endpoint_error", (output, labels)):
+            # TODO I don't think the two lines below is necessary.
+            # output = tf.to_float(output)
+            # labels = tf.to_float(labels)
+            output.get_shape().assert_is_compatible_with(labels.get_shape())
+
+            squared_difference = tf.square(tf.subtract(output, labels))
+            # sum across the 2 channels: sum[(X - Y)^2] -> N, H, W, 1
+            loss = tf.reduce_sum(squared_difference, axis=3, keep_dims=True)
+            loss = tf.sqrt(loss)
+            return tf.reduce_sum(loss) / batch_size
 
     def base(self, images, is_training, *args, **kwargs):
         """Base network.
@@ -192,18 +226,99 @@ class FlowNetSV1(BaseNetwork):
         concat2 = tf.concat([conv_2, deconv2, upsample_flow3], axis=3)
         predict_flow2 = self._predict_flow('predict_flow2', concat2)
 
-        # TODO Bilinar upsampling
-
+        # TODO Can I return a dict? What about if is training => dict {} else predict_flow2 ?
         return {
             'predict_flow6': predict_flow6,
             'predict_flow5': predict_flow5,
             'predict_flow4': predict_flow4,
             'predict_flow3': predict_flow3,
-            'predict_flow2': predict_flow2,
-            'flow': flow,
+            'predict_flow2': predict_flow2
+            # TODO do we need the flow below?
+            # 'flow': flow, W
         }
 
+    def placeholders(self):
+        """Placeholders.
 
+        Return placeholders.
+
+        Returns:
+            tf.placeholder: Placeholders.
+        """
+
+        shape = (self.batch_size, self.image_size[0], self.image_size[1], 3) \
+            if self.data_format == 'NHWC' else (self.batch_size, 3, self.image_size[0], self.image_size[1])
+        images_placeholder = tf.placeholder(
+            tf.float32,
+            shape=shape,
+            name="images_placeholder")
+
+        labels_placeholder = tf.placeholder(
+            # TODO check dataloader.py. I think it should be float32
+            tf.float32,
+            shape=(self.batch_size, self.image_size[0], self.image_size[1], 2),
+            name="labels_placeholder")
+
+        return images_placeholder, labels_placeholder
+
+    def inference(self, images, is_training):
+        base = self.base(images, is_training)
+        # TODO why do we need tf.identity?
+        # TODO I think separate is_training is necessary because train.py and predict.py both call inference.
+        if is_training:
+            return {k: tf.identity(v, name=k) for k,v in base.items()}
+        else:
+            predict_flow2 = base["predict_flow2"]
+            # TODO Bilinar upsampling
+            return tf.identity(predict_flow2, name="output")
+
+    # TODO output is a dict not a tensor.
+    def loss(self, output, labels):
+        """loss.
+
+        Params:
+           output: A dictionary of tensors.
+           Each tensor is a network output. shape is (batch_size, output_height, output_width, num_classes).
+           labels: Tensor of optical flow labels. shape is (batch_size, height, width, 2).
+        """
+
+        losses = []
+
+        # L2 loss between predict_flow6 (weighted w/ 0.32)
+        predict_flow6 = output['predict_flow6']
+        size = [predict_flow6.shape[1], predict_flow6.shape[2]]
+        downsampled_flow6 = self._downsample(labels, size)
+        losses.append(self._average_endpoint_error(downsampled_flow6, predict_flow6))
+
+        # L2 loss between predict_flow5 (weighted w/ 0.08)
+        predict_flow5 = output['predict_flow5']
+        size = [predict_flow5.shape[1], predict_flow5.shape[2]]
+        downsampled_flow5 = self._downsample(labels, size)
+        losses.append(self._average_endpoint_error(downsampled_flow5, predict_flow5))
+
+        # L2 loss between predict_flow4 (weighted w/ 0.02)
+        predict_flow4 = output['predict_flow4']
+        size = [predict_flow4.shape[1], predict_flow4.shape[2]]
+        downsampled_flow4 = self._downsample(labels, size)
+        losses.append(self._average_endpoint_error(downsampled_flow4, predict_flow4))
+
+        # L2 loss between predict_flow3 (weighted w/ 0.01)
+        predict_flow3 = output['predict_flow3']
+        size = [predict_flow3.shape[1], predict_flow3.shape[2]]
+        downsampled_flow3 = self._downsample(labels, size)
+        losses.append(self._average_endpoint_error(downsampled_flow3, predict_flow3))
+
+        # L2 loss between predict_flow2 (weighted w/ 0.005)
+        predict_flow2 = output['predict_flow2']
+        size = [predict_flow2.shape[1], predict_flow2.shape[2]]
+        downsampled_flow2 = self._downsample(labels, size)
+        losses.append(self._average_endpoint_error(downsampled_flow2, predict_flow2))
+
+        # This adds the weighted loss to the loss collection
+        tf.losses.compute_weighted_loss(losses, [0.32, 0.08, 0.02, 0.01, 0.005])
+
+        # Return the total loss: weighted loss + regularization terms defined in the model
+        return tf.losses.get_total_loss()
 
 
 
