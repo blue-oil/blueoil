86eab044f6f0f450eeeaebc24fd5a1e0ee0abf01
Yoshihiro Yamazaki <yoya@awm.jp>
2019-09-18 15:50:32 +0900

    add NPY reader.

--- runtime/src/blueoil_npy.cpp
+++ runtime/src/blueoil_npy.cpp
@@ -14,6 +14,13 @@ limitations under the License.
 =============================================================================*/
 
 #include <cassert>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+
 
 #include "blueoil.hpp"
 #include "blueoil_npy.hpp"
@@ -22,9 +29,200 @@ limitations under the License.
 namespace blueoil {
 namespace npy {
 
+struct NPYheader_t {
+  int depth;
+  int width, height;
+  int channels;
+};
+
+struct NPYheader_t readNPYheader(std::ifstream &fin);
+template<typename T>
+void readNPYimagedata(std::ifstream &fin, const struct NPYheader_t &nh,
+                      T *imagedata);
+
 Tensor Tensor_fromNPYFile(const std::string filename) {
-  blueoil::Tensor tensor({0});
+  std::ifstream fin(filename, std::ios::in | std::ios::binary);
+  std::stringstream ss;
+  if (!fin) {
+    ss << "Can't open file:" << filename;
+    throw std::runtime_error(ss.str());
+  }
+  NPYheader_t nh = readNPYheader(fin);
+  assert((nh.channels == 1) || (nh.channels == 3));  // grayscale or RGB
+  blueoil::Tensor tensor({nh.height, nh.width, nh.channels});
+  readNPYimagedata(fin, nh, tensor.dataAsArray());
   return tensor;
+}
+
+// " ABC " => "ABC"
+static std::string trim(const std::string str) {
+  const char *trimChara = " \t\r\n";
+  auto left = str.find_first_not_of(trimChara);
+  if (left  == std::string::npos) {
+    return "";
+  }
+  auto right = str.find_last_not_of(trimChara);
+  return str.substr(left, right - left + 1);
+}
+
+// "{...}" => "..."
+std::string extractInner(std::string strdata,
+                         std::string leftSep, std::string rightSep) {
+  auto braseFirstPos = strdata.find_first_of(leftSep);
+  auto braseLastPos = strdata.find_last_of(rightSep);
+  if ((braseFirstPos == std::string::npos) ||
+      (braseLastPos == std::string::npos)) {
+    return "";
+  }
+  return strdata.substr(braseFirstPos + 1, braseLastPos - braseFirstPos - 1);
+}
+std::string extractInner(std::string strdata, std::string sep) {
+  return extractInner(strdata, sep, sep);
+}
+
+// "A,B,(C,D,E)" => "A","B","(C,E,E)"
+std::vector<std::string> jsonCommaSplit(std::string strdata) {
+  std::vector<std::string> jsonVector;
+  std::string::size_type cur = 0, pos = 0;
+  for (cur = 0 ; cur < strdata.size() && (pos != std::string::npos) ; cur++) {
+    pos = strdata.find_first_of(",", cur);
+    if (pos == std::string::npos) {
+      pos = strdata.size();
+    }
+    auto bCur = strdata.find_first_of("(", cur);
+    if (bCur != std::string::npos) {
+      if (bCur < pos) {
+        auto bCur2 = strdata.find_first_of(")", bCur + 1);
+        if (bCur2 != std::string::npos) {
+          auto bCur3 = strdata.find_first_of(",", bCur2 + 1);
+          pos = bCur3;
+        } else {
+          throw std::runtime_error("can't closing bracket");
+        }
+      }
+    }
+    if (cur !=  pos) {
+      auto value = strdata.substr(cur, pos - cur);
+      jsonVector.push_back(trim(value));
+    }
+    cur = pos;
+  }
+  return jsonVector;
+}
+// "'A': B" =>  "A" => "B"
+std::pair<std::string, std::string> jsonKeyValueSplit(std::string strdata) {
+  auto pos = strdata.find_first_of(":", 0);
+  if (pos == std::string::npos) {
+    return std::pair<std::string, std::string>("", "");  // failed
+  }
+  std::string key = strdata.substr(0, pos);
+  std::string value = strdata.substr(pos + 1, strdata.size() - pos);
+  key  = trim(key);
+  value = trim(value);
+  auto tmp = extractInner(key, "'");
+  if (tmp.size() > 0) {
+    key = tmp;
+  }
+  tmp = extractInner(value, "'");
+  if (tmp.size() > 0) {
+    value = tmp;
+  }
+  return std::pair<std::string, std::string>(key, value);
+}
+
+// support only flat-1d assoc-array
+std::map<std::string, std::string> parseJson(std::string jsondata) {
+  std::map<std::string, std::string> jsonMap;
+  std::string innerBracesData = extractInner(jsondata, "{", "}");
+  // std::cerr << innerBracesData << std::endl;
+  std::vector<std::string> jsonList = jsonCommaSplit(innerBracesData);
+  for (auto jsonElem : jsonList) {
+    auto  keyvalue = jsonKeyValueSplit(jsonElem);
+      if (keyvalue.first.size() > 0) {
+        jsonMap[keyvalue.first] = keyvalue.second;
+      }
+  }
+  return jsonMap;
+}
+
+struct NPYheader_t readNPYheader(std::ifstream &fin) {
+  char sig[6];
+  uint16_t ver;
+  uint16_t jsonlen;
+  std::stringstream ss;
+  NPYheader_t header;
+  fin.read(sig, 6);
+  if (std::memcmp(sig, "\x93NUMPY", 6) != 0) {
+    ss << "wrong npy signature:" << sig;
+    throw std::runtime_error(ss.str());
+  }
+  fin.read(reinterpret_cast<char *>(&ver), sizeof(uint16_t));
+  fin.read(reinterpret_cast<char *>(&jsonlen), sizeof(uint16_t));
+  if ((* reinterpret_cast<const uint16_t*>("\1\0")) != 1) {
+    // native order = big endian
+    ver = (ver << 8) | (ver >> 8);
+    jsonlen = (jsonlen << 8) | (jsonlen >> 8);
+  }
+  if (0x80 < (0x0a + jsonlen)) {
+    ss << "too long json length:" << jsonlen;
+    throw std::runtime_error(ss.str());
+  }
+  std::string jsondata(jsonlen, '\0');
+  if (!fin.read(reinterpret_cast<char *>(&(jsondata[0])), jsonlen)) {
+    ss << "too short file for jsonlen:" << jsonlen;
+    throw std::runtime_error(ss.str());
+  }
+  // std::cerr << jsondata << std::endl;
+  // {'descr': '|u1', 'fortran_order': False, 'shape': (46, 70, 3), }
+  std::map<std::string, std::string> jsonMap = parseJson(jsondata);
+  for (auto itr = jsonMap.begin(); itr != jsonMap.end(); ++itr) {
+    std::string key = itr->first, value = itr->second;
+    if (key == "descr") {
+      if (value != "|u1") {
+        ss << "descr:" << value << ", must be lu1";
+        throw std::runtime_error(ss.str());
+      }
+      header.depth = 8;
+    } else if (key =="fortran_order") {
+      if (value != "False") {
+        throw std::runtime_error("fortran_order must be False");
+      }
+    } else if (key == "shape") {
+      value = extractInner(value, "(", ")");
+      std::vector<std::string> numstrList = jsonCommaSplit(value);
+      if (numstrList.size() != 3) {
+        ss << "Wrong shape size:" << numstrList.size();
+        throw std::runtime_error(ss.str());
+      }
+      header.height = std::stoi(numstrList[0]);
+      header.width = std::stoi(numstrList[1]);
+      header.channels = std::stoi(numstrList[2]);
+    } else {
+      ss << "Unknown json keye:" << key;
+      throw std::runtime_error(ss.str());
+    }
+  }
+  if (header.channels != 3) {
+    ss << "Wrong channels:" << header.channels;
+    throw std::runtime_error(ss.str());
+  }
+  return header;
+}
+
+template<typename T>
+void readNPYimagedata(std::ifstream &fin, const struct NPYheader_t &nh,
+                      T *imagedata) {
+  if (imagedata == NULL) {
+    throw std::runtime_error("imagedata == NULL");
+  }
+  int n = nh.width * nh.height * nh.channels;
+  for (int i = 0 ; i < n ; ++i) {
+    int cc = fin.get();
+    if (cc < 0) {
+      throw std::runtime_error("incompleted rgb-data");
+    }
+    imagedata[i] = cc;
+  }
 }
 
 }  // namespace npy
