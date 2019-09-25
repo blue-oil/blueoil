33fa8ced844516bfd25c2f2a7d244d1e87b43291
Yoshihiro Yamazaki <yoya@awm.jp>
2019-09-25 14:02:36 +0900

    fix comment, function name.

--- runtime/test/test_npy.cpp
+++ runtime/test/test_npy.cpp
@@ -37,13 +37,12 @@ float test_expect[3][3][3] =
      {  0, 255, 255},
     } };
 
-int test_opencv() {
-  // PNG24 using 9 colors
+int test_npy() {
   blueoil::Tensor input = blueoil::npy::Tensor_fromNPYFile("images/3x3colors.npy");
   blueoil::Tensor expect({3, 3, 3}, reinterpret_cast<float *>(test_expect));
   expect = blueoil::util::Tensor_CHW_to_HWC(expect);
   if (!input.allequal(expect)) {
-    std::cerr << "test_opencv: input != expect" << std::endl;
+    std::cerr << "test_npy: input != expect" << std::endl;
     blueoil::util::Tensor_HWC_to_CHW(input).dump();
     blueoil::util::Tensor_HWC_to_CHW(expect).dump();
     return EXIT_FAILURE;
@@ -53,7 +52,7 @@ int test_opencv() {
 
 int main(void) {
   int status_code = 0;
-  status_code = test_opencv();
+  status_code = test_npy();
   if (status_code != EXIT_FAILURE) {
     std::exit(status_code);
   }
