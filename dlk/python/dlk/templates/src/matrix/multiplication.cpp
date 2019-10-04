de1880bf6b8da8846febab1837a691c3de4e90ed
primenumber <--global>
2019-10-04 19:25:50 +0900

    Fix loop bound

--- dlk/python/dlk/templates/src/matrix/multiplication.cpp
+++ dlk/python/dlk/templates/src/matrix/multiplication.cpp
@@ -80,7 +80,7 @@ void matrix_multiplication_impl(
   for (std::size_t j = 0; j < B.cols(); j += regblock_m) {
     if (B.cols() - j >= regblock_m) {
       std::size_t k = 0;
-      for (; k < B.rows(); k += regblock_m) {
+      for (; k + regblock_m - 1 < B.rows(); k += regblock_m) {
         const auto im0 = vld1q_f32(B.data(k, j + 0));
         const auto im1 = vld1q_f32(B.data(k, j + 1));
         const auto im2 = vld1q_f32(B.data(k, j + 2));
