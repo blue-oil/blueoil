9836ec7416c6fc48d960e680ec5c0ef8d8d4a068
tk26eng <kota_58@yahoo.co.jp>
2019-10-18 20:33:55 +0900

    Save current status

--- dlk/hw/rootfs/build/make_rootfs.sh
+++ dlk/hw/rootfs/build/make_rootfs.sh
@@ -1,33 +1,14 @@
-FROM ubuntu:18.04
-
-#RUN apt-get update && apt-get install -y gcc-7-aarch64-linux-gnu
-RUN apt-get update && apt-get install -y gcc-8-aarch64-linux-gnu
-RUN apt-get install -y make
-RUN apt-get install -y sudo
-RUN apt-get install -y gcc
-RUN apt-get install -y bison
-RUN apt-get install -y flex
-RUN apt-get install -y device-tree-compiler
-
-# These are for ROOTFS of ubuntu
-RUN apt-get install -y debootstrap
-#RUN apt-get install -y vim
+#!/bin/bash
 
 # Add test user
 #RUN useradd test -u 1010
 #RUN echo 'test:testpass' | chpasswd
 #RUN echo "test  ALL=(ALL)  ALL" >> /etc/sudoers
 
-#RUN ln -s /usr/bin/aarch64-linux-gnu-gcc-7 /usr/local/bin/aarch64-linux-gnu-gcc 
-RUN ln -s /usr/bin/aarch64-linux-gnu-gcc-8 /usr/local/bin/aarch64-linux-gnu-gcc
-RUN ln -s /usr/bin/aarch64-linux-gnu-cpp-8 /usr/local/bin/aarch64-linux-gnu-cpp
-
 # Make rootfs
-RUN debootstrap --arch=arm64 --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg --verbose --foreign bionic /build/rootfs
-RUN apt-get install -y qemu-user-static
-RUN apt-get install -y vim
-RUN cp /usr/bin/qemu-aarch64-static /build/rootfs/usr/bin/
-COPY ./setting_after_chroot.sh /build/rootfs
-###RUN cp setting_after_chroot.sh /build/rootfs/
-#RUN chroot /build/rootfs /bin/bash /setting_after_chroot.sh
+debootstrap --arch=arm64 --keyring=/usr/share/keyrings/ubuntu-archive-keyring.gpg --verbose --foreign bionic /build/rootfs
+cp /usr/bin/qemu-aarch64-static /build/rootfs/usr/bin/
+#COPY ./setting_after_chroot.sh /build/rootfs
+cp /build/setting_after_chroot.sh /build/rootfs/
+chroot /build/rootfs /bin/bash /setting_after_chroot.sh
 
--- dlk/hw/rootfs/docker_run.sh
+++ dlk/hw/rootfs/docker_run.sh
@@ -1,1 +1,1 @@
-docker run -v build:/build/ -it --privileged ubuntu_make_img
+docker run -v build:/build/ -it --privileged ubuntu_make_img /build/make_rootfs.sh
