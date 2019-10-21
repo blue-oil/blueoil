f3e271b8d69d4a1fbd941ff309fab1a3de4d5523
tk26eng <kota_58@yahoo.co.jp>
2019-10-21 20:21:39 +0900

    Fix wrong part of the script

--- dlk/hw/rootfs/docker_run_rootfs32.sh
+++ dlk/hw/rootfs/docker_run_rootfs32.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`/build:/build/ -it --privileged ubuntu_make_img /build/make_rootfs.sh 64
+docker run -v `pwd`/build:/build/ -it --privileged ubuntu_make_img /build/make_rootfs.sh 32
