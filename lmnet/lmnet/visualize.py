6b09202014be05e6510d99a0946723201d6a2c42
yd8534976 <yd8534976@gmail.com>
2019-11-12 13:30:48 +0900

    Add comments for joint_pairs

--- lmnet/lmnet/visualize.py
+++ lmnet/lmnet/visualize.py
@@ -154,6 +154,12 @@ def visualize_keypoint_detection(image, joints, original_image_size=None):
 
     num_joints = joints.shape[0]
 
+    # These joints are paired to draw a skeleton.
+    # A line will be drew only if both of joints are visible.
+    # For details:
+    # {0: "nose", 1: "left_eye", 2: "right_eye", 3: "left_ear", 4: "right_ear",
+    #  5: "left_shoulder", 6: "right_shoulder", 7: "left_elbow", 8: "right_elbow", 9: "left_wrist", 10: "right_wrist",
+    #  11: "left_hip", 12: "right_hip", 13: "left_knee", 14: "right_knee", 15: "left_ankle", 16: "right_ankle"}
     joint_pairs = [[0, 1], [1, 3], [0, 2], [2, 4],
                    [5, 6], [5, 7], [7, 9], [6, 8], [8, 10],
                    [5, 11], [6, 12], [11, 12],
