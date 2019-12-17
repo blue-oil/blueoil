f23fa5ed08b20607ea4547af8204d1609e70c518
Hideaki Masuda <masuda@leapmind.io>
2019-12-17 18:35:44 +0900

    use dict comprehension

--- lmnet/executor/evaluate.py
+++ lmnet/executor/evaluate.py
@@ -150,7 +150,7 @@ def evaluate(config, restore_path, output_dir):
 
     metrics_dict = {
         'last_step': int(last_step),
-        'metrics': dict([[k, float(v)] for k, v in zip(list(metrics_ops_dict.keys()), metrics_values)]),
+        'metrics': {k: float(v) for k, v in zip(list(metrics_ops_dict.keys()), metrics_values)},
     }
     save_json(output_dir, json.dumps(metrics_dict, indent=4,), metrics_dict["last_step"])
 
