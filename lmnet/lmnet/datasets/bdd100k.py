4f2c1832a586b622bef008084fc50f84b12cc02a
Syed Hasibur Rahman <hasibur@leapmind.io>
2019-12-20 17:32:51 +0900

    Removing limiting validation data count for testing
    BDD100K dataset has 1000 images for validation which takes a long time to run validation. To test faster validation dataset has been reduced to 100 images. Removing this limitation for final code.

--- lmnet/lmnet/datasets/bdd100k.py
+++ lmnet/lmnet/datasets/bdd100k.py
@@ -139,10 +139,6 @@ class BDD100KObjectDetection(ObjectDetectionBase):
                 self.paths.append(img_paths[img_name])
                 self.bboxs.append(bbox)
 
-            if self.subset != 'train':
-                self.paths = self.paths[:100]
-                self.bboxs = self.bboxs[:100]
-
     def __getitem__(self, i, type=None):
         image_file_path = self.paths[i]
 
