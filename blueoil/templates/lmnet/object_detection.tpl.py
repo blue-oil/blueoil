f759f2f955f3e1b6d207d6de98dbb148ceebb151
Syed Hasibur Rahman <hasibur@leapmind.io>
2019-12-20 18:21:26 +0900

    Adding default cell size to config template
    Default cell size 32 is added to the object detection configuration template in blueoil

--- blueoil/templates/lmnet/object_detection.tpl.py
+++ blueoil/templates/lmnet/object_detection.tpl.py
@@ -47,6 +47,7 @@ DATASET_CLASS = type('DATASET_CLASS', ({{dataset_class}},), {{dataset_class_prop
 
 IMAGE_SIZE = {{image_size}}
 BATCH_SIZE = {{batch_size}}
+CELL_SIZE = 32
 DATA_FORMAT = "NHWC"
 TASK = Tasks.OBJECT_DETECTION
 CLASSES = {{classes}}
