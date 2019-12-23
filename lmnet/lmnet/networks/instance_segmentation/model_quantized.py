1d91e928a5d470e61a64c98d318739ece0cddadb
Lucien <lucienzhangzl@outlook.com>
2019-12-23 17:01:29 +0900

    try to use freeze session

--- lmnet/lmnet/networks/instance_segmentation/model_quantized.py
+++ lmnet/lmnet/networks/instance_segmentation/model_quantized.py
@@ -2258,15 +2258,43 @@ class MaskRCNN():
             workers=workers,
             use_multiprocessing=True,
         )
-        #######using session and saving .pb file##
-        # frozen_graph = self.freeze_session(K.get_session(),
-        #                                    output_names=[out.op.name for out in self.keras_model.outputs])
-        # tf.train.write_graph(frozen_graph, self.log_dir, "my_model.pb", as_text=False)
+        ######using session and saving .pb file##
+        frozen_graph = self.freeze_session(K.get_session(),
+                                           output_names=[out.op.name for out in self.keras_model.outputs])
+        tf.train.write_graph(frozen_graph, self.log_dir, "my_model.pb", as_text=False)
         #
         # pb_dir = os.path.join(self.log_dir, 'model_pb')
         # self.keras_model.save(str(pb_dir))
 
         self.epoch = max(self.epoch, epochs)
+
+    def freeze_session(self, session, keep_var_names=None, output_names=None, clear_devices=True):
+        """
+        Freezes the state of a session into a pruned computation graph.
+
+        Creates a new computation graph where variable nodes are replaced by
+        constants taking their current value in the session. The new graph will be
+        pruned so subgraphs that are not necessary to compute the requested
+        outputs are removed.
+        @param session The TensorFlow session to be frozen.
+        @param keep_var_names A list of variable names that should not be frozen,
+                              or None to freeze all the variables in the graph.
+        @param output_names Names of the relevant graph outputs.
+        @param clear_devices Remove the device directives from the graph for better portability.
+        @return The frozen graph definition.
+        """
+        graph = session.graph
+        with graph.as_default():
+            freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+            output_names = output_names or []
+            output_names += [v.op.name for v in tf.global_variables()]
+            input_graph_def = graph.as_graph_def()
+            if clear_devices:
+                for node in input_graph_def.node:
+                    node.device = ""
+            frozen_graph = tf.graph_util.convert_variables_to_constants(
+                session, input_graph_def, output_names, freeze_var_names)
+            return frozen_graph
 
     def save(self, save_path):
         self.keras_model.save(save_path)
