e73a9306a9f5c7923678f015cfaa2a044bfe3398
Hideaki Masuda <masuda@leapmind.io>
2019-12-24 12:45:18 +0900

    revert logger->print

--- lmnet/executor/profile_model.py
+++ lmnet/executor/profile_model.py
@@ -15,7 +15,6 @@
 # =============================================================================
 import collections
 import json
-import logging
 import os
 
 import click
@@ -24,10 +23,6 @@ import tensorflow as tf
 from lmnet import environment
 from lmnet.utils import config as config_util
 from lmnet.utils import executor
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 def _profile(config, restore_path, bit, unquant_layers):
     output_root_dir = os.path.join(environment.EXPERIMENT_DIR, "export")
@@ -62,7 +57,7 @@ def _profile(config, restore_path, bit, unquant_layers):
     sess.run(init_op)
 
     if restore_path:
-        logger.info("Restore from {}".format(restore_path))
+        print("Restore from {}".format(restore_path))
         saver.restore(sess, restore_path)
 
     main_output_dir = os.path.join(output_root_dir, "{}x{}".format(config.IMAGE_SIZE[0], config.IMAGE_SIZE[1]))
@@ -120,7 +115,7 @@ def _render(name, image_size, num_classes, bit, res):
     output_file = os.path.join(environment.EXPERIMENT_DIR, "{}_profile.md".format(name))
     with open(output_file, "w") as f:
         f.write(file_data)
-    logger.info("Model's profile has been saved into {}".format(output_file))
+    print("Model's profile has been saved into {}".format(output_file))
 
 
 def _save_json(name, image_size, num_classes, node_param_dict, node_flops_dict):
@@ -137,7 +132,7 @@ def _save_json(name, image_size, num_classes, node_param_dict, node_flops_dict):
     with open(output_file, "w") as f:
         f.write(json.dumps(prof_dict, indent=4))
 
-    logger.info("save json: {}".format(output_file))
+    print("save json: {}".format(output_file))
 
 
 def _profile_flops(graph, res, scopes):
