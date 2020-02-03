ee99a846163480d082dc3f156b36fb90879148ae
Joel Nicholls <32167300+joelN123@users.noreply.github.com>
2020-02-03 13:15:32 +0900

    close the validation dataset used in evaluate (#777)

--- lmnet/executor/evaluate.py
+++ lmnet/executor/evaluate.py
@@ -164,6 +164,7 @@ def evaluate(config, restore_path, output_dir):
         'metrics': {k: float(v) for k, v in zip(list(metrics_ops_dict.keys()), metrics_values)},
     }
     save_json(output_dir, json.dumps(metrics_dict, indent=4,), metrics_dict["last_step"])
+    validation_dataset.close()
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
