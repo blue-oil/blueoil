e02dd9eeb0ff2fdfe90ca7eb048e221a52ce1025
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2020-02-10 14:45:37 +0900

    Fix paths in blueoil

--- blueoil/templates/lmnet/classification.tpl.py
+++ blueoil/templates/lmnet/classification.tpl.py
@@ -28,7 +28,7 @@ from lmnet.pre_processor import (
     DivideBy255,
     PerImageStandardization
 )
-from blueoil.quantizations import (
+from blueoil.nn.quantizations import (
     binary_mean_scaling_quantizer,
     linear_mid_tread_half_quantizer,
 )
--- blueoil/templates/lmnet/object_detection.tpl.py
+++ blueoil/templates/lmnet/object_detection.tpl.py
@@ -33,7 +33,7 @@ from lmnet.post_processor import (
     ExcludeLowScoreBox,
     NMS,
 )
-from blueoil.quantizations import (
+from blueoil.nn.quantizations import (
     binary_channel_wise_mean_scaling_quantizer,
     linear_mid_tread_half_quantizer,
 )
