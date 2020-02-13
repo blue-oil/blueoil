b06be9268c3952babe74e6a72e3d1cc5963de785
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2020-02-13 11:08:30 +0900

    Unify 2 types of config format into lmnet config (#798)

--- tests/e2e/test_classification.py
+++ tests/e2e/test_classification.py
@@ -1,16 +1,138 @@
 import pytest
 
+from blueoil.generate_lmnet_config import generate
 from conftest import run_all_steps
 
 
+caltech101_classification = {
+    'model_name': 'caltech101_classification',
+    'task_type': 'classification',
+    'network_name': 'LmnetV1Quantize',
+    'network': {
+        'quantize_first_convolution': True
+    },
+    'dataset': {
+        'format': 'Caltech101',
+        'train_path': 'dummy_classification',
+        'test_path': None
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [128, 128],
+        'pretrain_model': False
+    }
+}
+
+
+caltech101_classification_has_validation = {
+    'model_name': 'caltech101_classification_has_validation',
+    'task_type': 'classification',
+    'network_name': 'LmnetV1Quantize',
+    'network': {
+        'quantize_first_convolution': True
+    },
+    'dataset': {
+        'format': 'Caltech101',
+        'train_path': 'dummy_classification',
+        'test_path': 'dummy_classification'
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [128, 128],
+        'pretrain_model': False
+    }
+}
+
+
+delta_mark_classification = {
+    'model_name': 'delta_mark_classification',
+    'task_type': 'classification',
+    'network_name': 'LmnetV1Quantize',
+    'network': {
+        'quantize_first_convolution': True
+    },
+    'dataset': {
+        'format': 'DeLTA-Mark for Classification',
+        'train_path': 'custom_delta_mark_classification/for_train',
+        'test_path': None
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [128, 128],
+        'pretrain_model': False
+    }
+}
+
+
+delta_mark_classification_has_validation = {
+    'model_name': 'delta_mark_classification_has_validation',
+    'task_type': 'classification',
+    'network_name': 'LmnetV1Quantize',
+    'network': {
+        'quantize_first_convolution': True
+    },
+    'dataset': {
+        'format': 'DeLTA-Mark for Classification',
+        'train_path': 'custom_delta_mark_classification/for_train',
+        'test_path': 'custom_delta_mark_classification/for_validation'
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [128, 128],
+        'pretrain_model': False
+    }
+}
+
+
 @pytest.mark.parametrize(
-    "config_file", [
-        "caltech101_classification.yml",
-        "caltech101_classification_has_validation.yml",
-        "delta_mark_classification.yml",
-        "delta_mark_classification_has_validation.yml",
+    "config", [
+        caltech101_classification,
+        caltech101_classification_has_validation,
+        delta_mark_classification,
+        delta_mark_classification_has_validation,
     ]
 )
-def test_classification(init_env, config_file):
+def test_classification(init_env, config):
     """Run Blueoil test of classification"""
+    config_file = generate(config)
     run_all_steps(init_env, config_file)
--- tests/e2e/test_keypoint_detection.py
+++ tests/e2e/test_keypoint_detection.py
@@ -1,14 +1,76 @@
 import pytest
 
+from blueoil.generate_lmnet_config import generate
 from conftest import run_all_steps
 
 
+mscoco2017_single_person_pose_estimation = {
+    'model_name': 'mscoco2017_single_person_pose_estimation',
+    'task_type': 'keypoint_detection',
+    'network_name': 'LmSinglePoseV1Quantize',
+    'network': {
+        'quantize_first_convolution': False
+    },
+    'dataset': {
+        'format': 'Mscoco for Single-Person Pose Estimation',
+        'train_path': 'MSCOCO_2017',
+        'test_path': None
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [160, 160],
+        'pretrain_model': False
+    }
+}
+
+
+mscoco2017_single_person_pose_estimation_has_validation = {
+    'model_name': 'mscoco2017_single_person_pose_estimation_has_validation',
+    'task_type': 'keypoint_detection',
+    'network_name': 'LmSinglePoseV1Quantize',
+    'network': {
+        'quantize_first_convolution': False
+    },
+    'dataset': {
+        'format': 'Mscoco for Single-Person Pose Estimation',
+        'train_path': 'MSCOCO_2017',
+        'test_path': 'MSCOCO_2017'
+    },
+    'trainer': {
+        'batch_size': 1,
+        'epochs': 1,
+        'initial_learning_rate': 0.001,
+        'keep_checkpoint_max': 5,
+        'learning_rate_schedule': 'constant',
+        'optimizer': 'Adam',
+        'save_checkpoint_steps': 1000
+    },
+    'common': {
+        'data_augmentation': {'Blur': [('value', (0, 1))]},
+        'dataset_prefetch': True,
+        'image_size': [160, 160],
+        'pretrain_model': False
+    }
+}
+
+
 @pytest.mark.parametrize(
-    "config_file", [
-        "mscoco2017_single_person_pose_estimation.yml",
-        "mscoco2017_single_person_pose_estimation_has_validation.yml",
+    "config", [
+        mscoco2017_single_person_pose_estimation,
+        mscoco2017_single_person_pose_estimation_has_validation,
     ]
 )
-def test_keypoint_detection(init_env, config_file):
+def test_keypoint_detection(init_env, config):
     """Run Blueoil test of keypoint detection"""
+    config_file = generate(config)
     run_all_steps(init_env, config_file)
