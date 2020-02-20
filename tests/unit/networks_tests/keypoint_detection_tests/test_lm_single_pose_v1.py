f6c82a03d64fd144a85c887b2568de774fdca96d
Wasin Kalintha <6939637+oatawa1@users.noreply.github.com>
2020-02-20 15:04:39 +0900

    Move blueoil/nn/quantizations to blueoil/quantizations (#862)
    * Move blueoil/nn/quantizations to blueoil/quantizations
    
    * Fixed PEP8 in test_yolo_v2_quantize.py
    
    * Fixed PEP8 (separate line for import line that greater that 79 characters)
    
    * Fixed PEP8 in segnet_quantize_bdd100k.py (suggest from lmnet-test)
    
    * Fixed PEP8 (trailing whitespace)

--- tests/unit/networks_tests/keypoint_detection_tests/test_lm_single_pose_v1.py
+++ tests/unit/networks_tests/keypoint_detection_tests/test_lm_single_pose_v1.py
@@ -28,7 +28,7 @@ from blueoil.pre_processor import (
     JointsToGaussianHeatmap
 )
 from blueoil.data_processor import Sequence
-from blueoil.nn.quantizations import (
+from blueoil.quantizations import (
     binary_channel_wise_mean_scaling_quantizer,
     linear_mid_tread_half_quantizer,
 )
