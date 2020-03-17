2abd9c77e4e7a87175e5a630dc3aaa15eebecb28
kurarrr <y.kurarrr@gmail.com>
2020-03-17 15:40:48 +0900

    add import tf

--- tests/unit/test_tfds_augmentor.py
+++ tests/unit/test_tfds_augmentor.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 # =============================================================================
 import numpy as np
+import tensorflow as tf
 import pytest
 
 from blueoil.tfds_augmentor import (
