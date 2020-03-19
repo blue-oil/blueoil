1e62a7db017ce101d832fa4d5cc96d42e34fdffc
Koichi Yoshigoe <yoshigoe@leapmind.io>
2020-03-19 20:55:45 +0900

    Fix checkpoint loader to use GFile

--- blueoil/cmd/train.py
+++ blueoil/cmd/train.py
@@ -395,8 +395,8 @@ def train(config_file, experiment_id=None, recreate=False):
     if not tf.io.gfile.exists(checkpoint):
         raise Exception('Checkpoints are not created in {}'.format(experiment_dir))
 
-    with open(checkpoint) as stream:
-        data = yaml.load(stream)
+    with tf.io.gfile.GFile(checkpoint) as stream:
+        data = yaml.load(stream, Loader=yaml.FullLoader)
     checkpoint_name = os.path.basename(data['model_checkpoint_path'])
 
     return experiment_id, checkpoint_name
