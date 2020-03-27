4c6687c6bb7f385134d0734182e4251ff8564256
Hideaki Masuda <masuda@leapmind.io>
2020-03-27 23:25:03 +0900

    fix assert output path

--- tests/device_tests/test_device_e2e.py
+++ tests/device_tests/test_device_e2e.py
@@ -47,7 +47,7 @@ class DeviceE2eTest(unittest.TestCase):
         sys.path.append(python_path)
         from run import run_prediction
         run_prediction(image, model, config)
-        assert os.path.exists(os.path.join(os.path.join(python_path, 'output'), "output.json"))
+        assert os.path.exists(os.path.join('output', "output.json"))
 
     def test_run(self):
         input_path = os.environ['DEVICE_TEST_INPUT_PATH']
