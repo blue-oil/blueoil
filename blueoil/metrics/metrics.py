14d57ab6a4dd5188bc01786f48a34d4e3f52a7b3
Yuta Fukasawa <30371920+ytfksw@users.noreply.github.com>
2020-04-10 12:35:12 +0900

    Apply tf_upgrade_v2 for metrics (#913)

--- blueoil/metrics/metrics.py
+++ blueoil/metrics/metrics.py
@@ -30,13 +30,13 @@ def tp_tn_fp_fn_for_each(output, labels, threshold=0.5):
 
     """
     predicted = tf.greater_equal(output, threshold)
-    gt_positive = tf.reduce_sum(tf.cast(labels, tf.int32), 0, keep_dims=True)
-    gt_negative = tf.reduce_sum(tf.cast(tf.logical_not(labels), tf.int32), 0, keep_dims=True)
+    gt_positive = tf.reduce_sum(tf.cast(labels, tf.int32), axis=0, keepdims=True)
+    gt_negative = tf.reduce_sum(tf.cast(tf.logical_not(labels), tf.int32), axis=0, keepdims=True)
     true_positive = tf.math.logical_and(predicted, labels)
-    true_positive = tf.reduce_sum(tf.cast(true_positive, tf.int32), 0, keep_dims=True)
+    true_positive = tf.reduce_sum(tf.cast(true_positive, tf.int32), axis=0, keepdims=True)
 
     true_negative = tf.math.logical_and(tf.logical_not(predicted), tf.math.logical_not(labels))
-    true_negative = tf.reduce_sum(tf.cast(true_negative, tf.int32), 0, keep_dims=True)
+    true_negative = tf.reduce_sum(tf.cast(true_negative, tf.int32), axis=0, keepdims=True)
     false_negative = gt_positive - true_positive
     false_positive = gt_negative - true_negative
 
