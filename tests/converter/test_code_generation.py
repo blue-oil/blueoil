fbcb6a8593f87082b8a58ec46c538cd2b681da8f
Taketoshi Fujiwara <fujiwara@leapmind.io>
2020-04-14 00:00:24 +0900

    Fix syntax error

--- tests/converter/test_code_generation.py
+++ tests/converter/test_code_generation.py
@@ -286,7 +286,7 @@ class TestCodeGenerationBase(TestCaseDLKBase):
         pf = 100.0
         try:
             pf = float(remote_output)
-        except TypeError, ValueError:
+        except (TypeError, ValueError):
             pf = 100.0
 
         return pf
