e4d12944be3562c6eecc1250f8a5384278c18ec0
odoku <m.onogawa@gmail.com>
2020-05-26 16:29:52 +0900

    Added "data_dir" and "output_dir" options to the command.

--- blueoil/environment.py
+++ blueoil/environment.py
@@ -15,15 +15,47 @@
 # =============================================================================
 import os
 
+EXPERIMENT_ID = None
+EXPERIMENT_DIR = None
+TENSORBOARD_DIR = None
+CHECKPOINTS_DIR = None
+
 default_data_dir = "dataset"
 DATA_DIR = os.getenv("DATA_DIR", os.path.join(os.getcwd(), default_data_dir))
 
 default_output_dir = "saved"
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", default_output_dir)
+OUTPUT_DIR = os.getenv("OUTPUT_DIR", os.path.join(os.getcwd(), default_output_dir))
 
 _EXPERIMENT_DIR = os.path.join(OUTPUT_DIR, "{experiment_id}")
 _TENSORBOARD_DIR = os.path.join(OUTPUT_DIR, "{experiment_id}", "tensorboard")
 _CHECKPOINTS_DIR = os.path.join(OUTPUT_DIR, "{experiment_id}", "checkpoints")
+
+
+def set_data_dir(path):
+    global DATA_DIR
+    DATA_DIR = os.path.abspath(path)
+    os.environ["DATA_DIR"] = DATA_DIR
+
+
+def set_output_dir(path):
+    global OUTPUT_DIR
+    OUTPUT_DIR = os.path.abspath(path)
+    os.environ["OUTPUT_DIR"] = OUTPUT_DIR
+
+    global _EXPERIMENT_DIR, _TENSORBOARD_DIR, _CHECKPOINTS_DIR
+    _EXPERIMENT_DIR = os.path.join(
+        OUTPUT_DIR, "{experiment_id}",
+    )
+    _TENSORBOARD_DIR = os.path.join(
+        OUTPUT_DIR, "{experiment_id}", "tensorboard",
+    )
+    _CHECKPOINTS_DIR = os.path.join(
+        OUTPUT_DIR, "{experiment_id}", "checkpoints",
+    )
+
+    global EXPERIMENT_ID
+    if EXPERIMENT_ID:
+        init(EXPERIMENT_ID)
 
 
 def init(experiment_id):
@@ -31,12 +63,12 @@ def init(experiment_id):
 
     experiment id embed to directories.
     """
-    global EXPERIMENT_DIR, TENSORBOARD_DIR, CHECKPOINTS_DIR
+    global EXPERIMENT_ID, EXPERIMENT_DIR, TENSORBOARD_DIR, CHECKPOINTS_DIR
 
     # remove OUTPUT_DIR if it be included in experiment_id.
     if OUTPUT_DIR + os.path.sep in experiment_id:
         experiment_id = experiment_id.replace(OUTPUT_DIR + os.path.sep, "")
-        print(experiment_id)
+    EXPERIMENT_ID = experiment_id
 
     EXPERIMENT_DIR = _EXPERIMENT_DIR.format(experiment_id=experiment_id)
 
---
+++ tests/unit/test_environment.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Blueoil Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+import os
+
+import pytest
+
+from blueoil import environment
+
+
+class EnvironmentTestBase(object):
+    def setup_method(self, method):
+        self.default_env = {
+            "EXPERIMENT_ID": environment.EXPERIMENT_ID,
+            "EXPERIMENT_DIR": environment.EXPERIMENT_DIR,
+            "TENSORBOARD_DIR": environment.TENSORBOARD_DIR,
+            "CHECKPOINTS_DIR": environment.CHECKPOINTS_DIR,
+            "DATA_DIR": environment.DATA_DIR,
+            "OUTPUT_DIR": environment.OUTPUT_DIR,
+            "_EXPERIMENT_DIR": environment._EXPERIMENT_DIR,
+            "_TENSORBOARD_DIR": environment._TENSORBOARD_DIR,
+            "_CHECKPOINTS_DIR": environment._CHECKPOINTS_DIR,
+        }
+        environment.EXPERIMENT_ID = None
+        environment.EXPERIMENT_DIR = None
+        environment.TENSORBOARD_DIR = None
+        environment.CHECKPOINTS_DIR = None
+        environment.DATA_DIR = os.getenv(
+            "DATA_DIR", os.path.join(os.getcwd(), "dataset"),
+        )
+        environment.OUTPUT_DIR = os.getenv(
+            "OUTPUT_DIR", os.path.join(os.getcwd(), "saved"),
+        )
+        environment._EXPERIMENT_DIR = os.path.join(
+            environment.OUTPUT_DIR, "{experiment_id}",
+        )
+        environment._TENSORBOARD_DIR = os.path.join(
+            environment.OUTPUT_DIR, "{experiment_id}", "tensorboard",
+        )
+        environment._CHECKPOINTS_DIR = os.path.join(
+            environment.OUTPUT_DIR, "{experiment_id}", "checkpoints",
+        )
+
+    def teardown_method(self, method):
+        environment.EXPERIMENT_ID = self.default_env["EXPERIMENT_ID"]
+        environment.EXPERIMENT_DIR = self.default_env["EXPERIMENT_DIR"]
+        environment.TENSORBOARD_DIR = self.default_env["TENSORBOARD_DIR"]
+        environment.CHECKPOINTS_DIR = self.default_env["CHECKPOINTS_DIR"]
+        environment.DATA_DIR = self.default_env["DATA_DIR"]
+        environment.OUTPUT_DIR = self.default_env["OUTPUT_DIR"]
+        environment._EXPERIMENT_DIR = self.default_env["_EXPERIMENT_DIR"]
+        environment._TENSORBOARD_DIR = self.default_env["_TENSORBOARD_DIR"]
+        environment._CHECKPOINTS_DIR = self.default_env["_CHECKPOINTS_DIR"]
+
+
+class TestInit(EnvironmentTestBase):
+    def test_init(self):
+        experiment_id = "experiment001"
+        environment.init(experiment_id)
+        assert environment.EXPERIMENT_ID == experiment_id
+        assert environment.EXPERIMENT_DIR == os.path.join(
+            environment.OUTPUT_DIR, experiment_id,
+        )
+        assert environment.TENSORBOARD_DIR == os.path.join(
+            environment.OUTPUT_DIR, experiment_id, "tensorboard",
+        )
+        assert environment.CHECKPOINTS_DIR == os.path.join(
+            environment.OUTPUT_DIR, experiment_id, "checkpoints",
+        )
+
+    def test_init_with_experiment_dir(self):
+        experiment_id = "experiment001"
+        experiment_dir = os.path.join(environment.OUTPUT_DIR, experiment_id)
+        environment.init(experiment_dir)
+        assert environment.EXPERIMENT_ID == experiment_id
+        assert environment.EXPERIMENT_DIR == experiment_dir
+        assert environment.TENSORBOARD_DIR == os.path.join(
+            experiment_dir, "tensorboard",
+        )
+        assert environment.CHECKPOINTS_DIR == os.path.join(
+            experiment_dir, "checkpoints",
+        )
+
+
+class TestSetDataDir(EnvironmentTestBase):
+    def test_set_data_dir(self):
+        path = "./dataset"
+        environment.set_data_dir(path)
+
+        abs_path = os.path.abspath(path)
+        assert environment.DATA_DIR == abs_path
+        assert os.environ["DATA_DIR"] == abs_path
+
+
+class TestSetOutputDir(EnvironmentTestBase):
+    def test_set_output_dir(self):
+        path = "./output"
+        environment.set_output_dir(path)
+
+        abs_path = os.path.abspath(path)
+        assert environment.OUTPUT_DIR == abs_path
+        assert os.environ["OUTPUT_DIR"] == abs_path
+
+        assert environment._EXPERIMENT_DIR == os.path.join(
+            abs_path, "{experiment_id}",
+        )
+        assert environment._TENSORBOARD_DIR == os.path.join(
+            abs_path, "{experiment_id}", "tensorboard",
+        )
+        assert environment._CHECKPOINTS_DIR == os.path.join(
+            abs_path, "{experiment_id}", "checkpoints",
+        )
+        assert environment.EXPERIMENT_DIR is None
+        assert environment.TENSORBOARD_DIR is None
+        assert environment.CHECKPOINTS_DIR is None
+
+    def test_set_output_dir_after_init(self):
+        experiment_id = "experiment001"
+        environment.init(experiment_id)
+
+        path = "./output"
+        environment.set_output_dir(path)
+
+        abs_path = os.path.abspath(path)
+        assert environment.EXPERIMENT_DIR == os.path.join(
+            abs_path, experiment_id,
+        )
+        assert environment.TENSORBOARD_DIR == os.path.join(
+            abs_path, experiment_id, "tensorboard",
+        )
+        assert environment.CHECKPOINTS_DIR == os.path.join(
+            abs_path, experiment_id, "checkpoints",
+        )
