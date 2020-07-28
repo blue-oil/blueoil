3448f0dbd28a5c40d7f83fbf307dc8f03682bdcf
Wasin Kalintha <6939637+oatawa1@users.noreply.github.com>
2020-07-28 16:13:56 +0900

    Add blank line between copyright header and code in file with more comments or # NOQA (#1133)

--- blueoil/networks/segmentation/__init__.py
+++ blueoil/networks/segmentation/__init__.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+
 from .lm_segnet_v1 import LmSegnetV1, LmSegnetV1Quantize  # NOQA
 
 LmSegnet = LmSegnetV1
