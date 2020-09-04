bc61ddf02f92f9016dbc7d4f5f99ff6bd6b7b743
Hideaki Masuda <masuda@leapmind.io>
2020-09-04 10:26:35 +0900

    enable to output GCS with pretrain

--- blueoil/cmd/train.py
+++ blueoil/cmd/train.py
@@ -123,11 +123,11 @@ def start_training(config, profile_step):
 
         saver = tf.compat.v1.train.Saver(max_to_keep=config.KEEP_CHECKPOINT_MAX)
 
-        with open(os.path.join(environment.EXPERIMENT_DIR, "pretrain_vars.txt"), 'w') as pretrain_vars_file:
+        with tf.io.gfile.GFile(os.path.join(environment.EXPERIMENT_DIR, "pretrain_vars.txt"), 'w') as pretrain_vars_file:
             train_vars = tf.compat.v1.trainable_variables()
-            pretrain_vars_file.writelines("[\n")
-            pretrain_vars_file.writelines("    '%s',\n" % var.name for var in train_vars)
-            pretrain_vars_file.writelines("]\n")
+            pretrain_vars_file.write("[\n")
+            pretrain_vars_file.write("".join("    '%s',\n" % var.name for var in train_vars))
+            pretrain_vars_file.write("]\n")
 
         if config.IS_PRETRAIN:
             all_vars = tf.compat.v1.global_variables()
