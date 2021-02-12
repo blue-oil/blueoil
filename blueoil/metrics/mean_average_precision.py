9770a85c64b03899f1a4baaf5538cde0b2fe2b8d
Taketoshi Fujiwara <fujiwara@leapmind.io>
2021-02-12 12:32:34 +0900

    Fix license texts for third-party code (#1237)
    ## What this patch does to fix the issue.
    - `blueoil/common.py`: remove unnecessary comment
    - `blueoil/data_augmentor.py`: add copyright info for third-party code
    - `blueoil/metrics/mean_average_precision.py`: add copyright info for third-party code
    - `blueoil/turbo_color_map.py`: remove Blueoil's copyright because whole the file comes from third-party code

--- blueoil/metrics/mean_average_precision.py
+++ blueoil/metrics/mean_average_precision.py
@@ -25,6 +25,20 @@ from tensorflow.python.ops import metrics_impl, variable_scope
 # ===========================================================================
 # Average precision computations on tensorflow
 # Ref: https://github.com/balancap/SSD-Tensorflow/blob/master/tf_extended/metrics.py
+#
+# Copyright 2017 Paul Balanca. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # ===========================================================================
 def average_precision(
         num_gt_boxes,
